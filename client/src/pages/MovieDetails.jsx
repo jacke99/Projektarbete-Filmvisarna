74617807ecb7158323fde613a253214fedeaa7cd
jacob von scheele <jacobvonscheele@outlook.com>
2023-09-27 15:57:53 +0200

    added mobile styling for movieDetails page

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -1,3 +1,67 @@
+import { killersPoster, killersImage } from "../assets/index.js";
+import { styles } from "../styles.js";
 export default function MovieDetails() {
-  return <div>MovieDetails</div>;
+  return (
+    <div className=" h-full bg-primary">
+      <div className="relative h-96">
+        <img
+          src={killersImage}
+          alt="movie poster"
+          className="h-full max-w-full object-cover"
+        />
+        <img
+          src={killersPoster}
+          alt="movie poster"
+          className="  md: absolute bottom-28 left-5 block h-44 sm:hidden"
+        />
+        <div
+          className={`absolute bottom-7 left-5 rounded bg-primary bg-opacity-60 p-1`}
+        >
+          <p className={`${styles.trailerTitle} `}>Lorem Ipsum</p>
+          <p className={`${styles.trailerSubTitle} `}>Romantica, Comadia</p>
+          <p className={`${styles.trailerSubTitle} font-inconsolata bold`}>
+            1 tim 33min | 11 år
+          </p>
+        </div>
+      </div>
+      <div className="relative flex flex-col">
+        <p className={`p-8 text-white`}>
+          Lorem Ipsum è un testo segnaposto utilizzato nel settore della
+          tipografia e della stampa. Lorem Ipsum è considerato il testo
+          segnaposto standard sin dal sedicesimo secolo, quando un anonimo
+          tipografo prese una cassetta di caratteri e li assemblò per preparare
+          un testo campione. quando un anonimo tipografo prese una cassetta di
+          caratteri e li assemblò per preparare un.
+        </p>
+        <button className=" m-auto mb-4 rounded-xl bg-gold px-4 py-2 text-xl text-black">
+          Biljetter
+        </button>
+        <div className="px-8 py-2 text-white">
+          <span>Regi:</span>
+          <span>Lorem Ipsum</span>
+        </div>
+        <div className="px-8 py-2 text-white">
+          <span>Skådespelare:</span>
+          <span>Testo segnaposto, Testo segnaposto</span>
+        </div>
+        <div className="px-8 py-2 text-white">
+          <span>Originaltitel:</span>
+          <span>testo segnaposto</span>
+        </div>
+        <div className="px-8 py-2 text-white">
+          <span>Premiär:</span>
+          <span>22 sep 2023</span>
+        </div>
+        <div className="mb-8 px-8 py-2 text-white">
+          <span>Originalspråk</span>
+          <span>Lorem Ipsum</span>
+        </div>
+        <img
+          src={killersPoster}
+          alt="movie poster"
+          className=" absolute bottom-8 right-8 hidden h-44 w-44 sm:block"
+        />
+      </div>
+    </div>
+  );
 }
