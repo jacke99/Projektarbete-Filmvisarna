a766770beb37f0fb2904878932d20d3f20527a82
marcus nilsson <marcusnilsson2010@gmail.com>
2023-09-27 15:58:21 +0200

    working on register page, everything is fine except background img

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,30 +1,27 @@
-import aLitterLifeImage from "./aLitterLifeImage.png"
-import aLitterLifePoster from "./aLitterLifePoster.png"
-import logo from "./filmvisarna-logo.png"
-import killersImage from "./killersImage.jpg"
-import killersPoster from "./killersPoster.jpg"
-import pastLivesImage from "./pastLivesImage.png"
-import pastLivesPoster from "./pastLivesPoster.jpg"
-import smsImage from "./smsImage.png"
-import smsPoster from "./smsPoster.png"
-import theCreatorImage from "./theCreatorImage.png"
-import theCreatorPoster from "./theCreatorPoster.png"
-
-
-
-
-
+import aLittleLifeImage from "./aLittleLifeImage.png";
+import aLittleLifePoster from "./aLittleLifePoster.png";
+import logo from "./filmvisarna-logo.png";
+import killersImage from "./killersImage.jpg";
+import killersPoster from "./killersPoster.jpg";
+import pastLivesImage from "./pastLivesImage.png";
+import pastLivesPoster from "./pastLivesPoster.png";
+import smsImage from "./smsImage.png";
+import smsPoster from "./smsPoster.png";
+import theCreatorImage from "./theCreatorImage.png";
+import theCreatorPoster from "./theCreatorPoster.jpg";
+import chairs from "./chairs.jpg"
 
 export {
-    aLitterLifeImage,
-    aLitterLifePoster,
-    logo,
-    killersImage,
-    killersPoster,
-    pastLivesImage,
-    pastLivesPoster,
-    smsImage,
-    smsPoster,
-    theCreatorImage,
-    theCreatorPoster
-}
+  aLittleLifeImage,
+  aLittleLifePoster,
+  logo,
+  killersImage,
+  killersPoster,
+  pastLivesImage,
+  pastLivesPoster,
+  smsImage,
+  smsPoster,
+  theCreatorImage,
+  theCreatorPoster,
+  chairs
+};
