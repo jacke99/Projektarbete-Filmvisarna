a487da006896f677e7be07b2ea8fd2c074bad862
HolaCarmensita <Casandra.c.gustafsson@gmail.com>
2023-09-27 15:53:13 +0200

    component Carousel and MovieImgBlock
    I only had time to start using tailwind. Made the components.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,32 @@
-import { styles } from "../styles.js";
+import Header from "../components/Header.jsx";
+import {
+  killersImage,
+  theCreatorPoster,
+  killersPoster,
+  theCreatorImage,
+  pastLivesImage,
+} from "../assets/index.js";
+
+import MovieImgBlock from "../components/MovieImgBlock.jsx";
+import Carousel from "../components/Carousel.jsx";
 
 export default function Home() {
   return (
-    <div>
-      <h2 className={`${styles.movieTitle} `}>Header</h2>
-      <p>Text</p>
-    </div>
+    <>
+      <Header />
+      <main>
+        <MovieImgBlock movieImg={killersImage} />
+        <Carousel
+          moviePoster1={theCreatorPoster}
+          moviePoster2={killersPoster}
+        />
+        <MovieImgBlock movieImg={theCreatorImage} />
+        <Carousel
+          moviePoster1={theCreatorPoster}
+          moviePoster2={killersPoster}
+        />
+        <MovieImgBlock movieImg={pastLivesImage} />
+      </main>
+    </>
   );
 }
