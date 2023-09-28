829c951a4349b872cf412f68a38cf606324bfd1c
marcus nilsson <marcusnilsson2010@gmail.com>
2023-09-28 14:13:51 +0200

    form is now responsive with email and password. Will start working on first namn and lastname

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,35 +1,31 @@
-import aLitterLifeImage from "./aLitterLifeImage.png"
-import aLitterLifePoster from "./aLitterLifePoster.png"
-import logo from "./filmvisarna-logo.png"
-import killersImage from "./killersImage.jpg"
-import killersPoster from "./killersPoster.jpg"
-import pastLivesImage from "./pastLivesImage.png"
-import pastLivesPoster from "./pastLivesPoster.jpg"
-import smsImage from "./smsImage.png"
-import smsPoster from "./smsPoster.png"
-import theCreatorImage from "./theCreatorImage.png"
-import theCreatorPoster from "./theCreatorPoster.png"
+import aLittleLifeImage from "./aLittleLifeImage.png";
+import aLittleLifePoster from "./aLittleLifePoster.png";
+import logo from "./logo.png";
+import killersImage from "./killersImage.jpg";
+import killersPoster from "./killersPoster.jpg";
+import pastLivesImage from "./pastLivesImage.png";
+import pastLivesPoster from "./pastLivesPoster.png";
+import smsImage from "./smsImage.png";
+import smsPoster from "./smsPoster.png";
+import theCreatorImage from "./theCreatorImage.png";
+import theCreatorPoster from "./theCreatorPoster.jpg";
 import menu from "./menu.svg";
 import account_circle from "./account_circle.svg"
 import close from "./close.svg"
 
-
-
-
-
 export {
-    aLitterLifeImage,
-    aLitterLifePoster,
-    logo,
-    killersImage,
-    killersPoster,
-    pastLivesImage,
-    pastLivesPoster,
-    smsImage,
-    smsPoster,
-    theCreatorImage,
-    theCreatorPoster,
-    menu,
-    close,
-    account_circle,
-}
+  aLittleLifeImage,
+  aLittleLifePoster,
+  logo,
+  menu,
+  close,
+  account_circle,
+  killersImage,
+  killersPoster,
+  pastLivesImage,
+  pastLivesPoster,
+  smsImage,
+  smsPoster,
+  theCreatorImage,
+  theCreatorPoster,
+}; 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -5,19 +5,19 @@ import { styles } from "../styles";
 export default function Register() {
   return <div className= "bg-[url('./assets/chairs.jpg')] bg-cover bg-center " >
     <header>
-      <h1 className={`${styles.headerText} text-white text-center  p-10  text-4xl`}>Bli medlem</h1>
+      <h1 className={`${styles.headerText} text-white text-center  pt-40 pb-10  text-4xl`}>Bli medlem</h1>
       
     </header>
     
-    <form className="flex flex-col items-center">
-      <div>
-        <input className={`${styles.regInputs}`} type="text" id="fname" name="fname" placeholder="Förnamn..."/>
-        <input className={`${styles.regInputs} `} type="text" id="fname" name="fname" placeholder="Efternamn..."/>
+    <form className="flex flex-col items-center border-2 border-indigo-600  w-screen md:w-2/3 m-auto lg:w-2/4" >
+      <div className="">
+        <input className type="text" id="fname" name="fname" placeholder="Förnamn..."/>
+        <input className type="text" id="fname" name="fname" placeholder="Efternamn..."/>
       </div>
       
-      <input className={`${styles.regInputs} w-80 `} type="email" id="email" name="email" placeholder="Epost..."/>
-      <input className={`${styles.regInputs} w-80`} type="password" id="password" name="password" placeholder="Lösenord..."/>
-      <input className={`${styles.regInputs} w-80`} type="password" id="confirm_password" name="confirm_password" placeholder="Bekräfta lösenord..."/>
+      <input className={`${styles.regInputs} w-[80%]  `} type="email" id="email" name="email" placeholder="Epost..."/>
+      <input className={`${styles.regInputs} w-[80%] `} type="password" id="password" name="password" placeholder="Lösenord..."/>
+      <input className={`${styles.regInputs} w-[80%]  `} type="password" id="confirm_password" name="confirm_password" placeholder="Bekräfta lösenord..."/>
       <button className="bg-gold w-40 px-4 rounded-lg py-4 my-5 " >Registrera</button>
 
     </form >
