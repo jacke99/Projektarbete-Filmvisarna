0bb5935a86a09f3337294b8e80bff0455126c8af
marcus nilsson <marcusnilsson2010@gmail.com>
2023-09-28 14:36:29 +0200

    form (ink ) button and font is now also responsive. Form is now ready to mege

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -9,7 +9,7 @@ export default function Register() {
       
     </header>
     
-    <form className="flex flex-col items-center border-2 border-indigo-600  w-screen md:w-2/3 m-auto lg:w-2/4" >
+    <form className="flex flex-col items-center border-2 border-indigo-600  w-screen md:w-2/3 m-auto lg:w-2/4 lg:text-lg"  >
       <div className=" flex justify-center lg:w-[100%] md:w-[100%] sm:w-[100%] ">
         <input className={`${styles.regInputs} w-[41%] `} type="text" id="fname" name="fname" placeholder="Förnamn..."/>
         <input className={`${styles.regInputs} w-[41%] `} type="text" id="fname" name="fname" placeholder="Efternamn..."/>
@@ -18,7 +18,7 @@ export default function Register() {
       <input className={`${styles.regInputs} w-[85%]  `} type="email" id="email" name="email" placeholder="Epost..."/>
       <input className={`${styles.regInputs} w-[85%] `} type="password" id="password" name="password" placeholder="Lösenord..."/>
       <input className={`${styles.regInputs} w-[85%]  `} type="password" id="confirm_password" name="confirm_password" placeholder="Bekräfta lösenord..."/>
-      <button className="bg-gold w-40 px-4 rounded-lg py-4 my-5 " >Registrera</button>
+      <button className="bg-gold w-40 px-4 rounded-lg py-4 my-5 lg:text-lg " >Registrera</button>
 
     </form >
 
