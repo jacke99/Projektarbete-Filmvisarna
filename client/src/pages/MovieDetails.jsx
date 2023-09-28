99f735c2993fa635b1862bf0905d7142be7cd7b3
jacob von scheele <jacobvonscheele@outlook.com>
2023-09-28 15:49:48 +0200

    made page responsive for mobile, tablet and desktop

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -7,7 +7,7 @@ export default function MovieDetails() {
         <img
           src={killersImage}
           alt="movie poster"
-          className="h-full max-w-full object-cover"
+          className=" h-full min-w-full object-cover"
         />
         <img
           src={killersPoster}
@@ -15,7 +15,7 @@ export default function MovieDetails() {
           className="  md: absolute bottom-28 left-5 block h-44 sm:hidden"
         />
         <div
-          className={`absolute bottom-7 left-5 rounded bg-primary bg-opacity-60 p-1`}
+          className={`absolute bottom-7 left-5 rounded bg-primary bg-opacity-60 p-1 lg:left-24`}
         >
           <p className={`${styles.trailerTitle} `}>Lorem Ipsum</p>
           <p className={`${styles.trailerSubTitle} `}>Romantica, Comadia</p>
@@ -24,8 +24,8 @@ export default function MovieDetails() {
           </p>
         </div>
       </div>
-      <div className="relative flex flex-col">
-        <p className={`p-8 text-white`}>
+      <div className="relative m-auto flex flex-col p-8 sm:p-12 md:w-5/6 lg:w-2/3 lg:pb-8 xl:w-3/5 2xl:w-3/6">
+        <p className={` mb-8 text-white sm:text-xl `}>
           Lorem Ipsum è un testo segnaposto utilizzato nel settore della
           tipografia e della stampa. Lorem Ipsum è considerato il testo
           segnaposto standard sin dal sedicesimo secolo, quando un anonimo
@@ -33,33 +33,33 @@ export default function MovieDetails() {
           un testo campione. quando un anonimo tipografo prese una cassetta di
           caratteri e li assemblò per preparare un.
         </p>
-        <button className=" m-auto mb-4 rounded-xl bg-gold px-4 py-2 text-xl text-black">
+        <button className=" m-auto mb-4 rounded-xl bg-gold px-4 py-2 text-xl text-black sm:px-5 sm:text-2xl">
           Biljetter
         </button>
-        <div className="px-8 py-2 text-white">
+        <div className={`${styles.movieDescInfo}`}>
           <span>Regi:</span>
           <span>Lorem Ipsum</span>
         </div>
-        <div className="px-8 py-2 text-white">
+        <div className={`${styles.movieDescInfo}`}>
           <span>Skådespelare:</span>
           <span>Testo segnaposto, Testo segnaposto</span>
         </div>
-        <div className="px-8 py-2 text-white">
+        <div className={`${styles.movieDescInfo}`}>
           <span>Originaltitel:</span>
           <span>testo segnaposto</span>
         </div>
-        <div className="px-8 py-2 text-white">
+        <div className={`${styles.movieDescInfo}`}>
           <span>Premiär:</span>
           <span>22 sep 2023</span>
         </div>
-        <div className="mb-8 px-8 py-2 text-white">
+        <div className={`${styles.movieDescInfo}`}>
           <span>Originalspråk</span>
           <span>Lorem Ipsum</span>
         </div>
         <img
           src={killersPoster}
           alt="movie poster"
-          className=" absolute bottom-8 right-8 hidden h-44 w-44 sm:block"
+          className=" absolute bottom-8 right-12 hidden h-56 sm:block "
         />
       </div>
     </div>
--- client/src/styles.js
+++ client/src/styles.js
@@ -6,8 +6,10 @@ const styles = {
   subHeaderText: "text-gold sm:text-[16px] md:text-[25px] lg:text-[30px]",
   movieTitle:
     "text-white-100 font-inconsolata  sm:text-[15px] md:text-[20px] lg:text-[25px]",
-  trailerTitle: "text-gold sm:text-[20px] md:text-[35px] lg:text-[40px]",
-  trailerSubTitle: "text-gold sm:text-[13px] md:text-[20px] lg:text-[25px]",
+  trailerTitle: "text-gold sm:text-[20px] md:text-[30px] lg:text-[35px]",
+  trailerSubTitle: "text-gold sm:text-[13px] md:text-[18px] lg:text-[20px]",
+  movieDescInfo:
+    "text-white-100 sm:text-[12px] md:text-[15px] lg:text-[20px] flex flex-col py-2",
 };
 
 export { styles };
