f720b0fdc18335e7bc31cd019d87dec1f88d11a5
marcus nilsson <marcusnilsson2010@gmail.com>
2023-09-28 16:02:54 +0200

    adding 'bli en del av familjen to registration ', will work on gradient tomorrow.

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -4,12 +4,21 @@ import { styles } from "../styles";
 
 export default function Register() {
   return <div className= "bg-[url('./assets/chairs.jpg')] bg-cover bg-center " >
+    
+    <div className="" >
+      <h2 className={`${styles.headerText} text-white text-center  pt-40 pb-10  text-4xl`}>
+        Bli en del av familjen!
+      </h2>
+      <p className=" text-white text-center w-[41%] m-auto lg:text-lg">Välkommen till oss på Filmvisarna. Vi älskar film! Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia
+         e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, quando un anonimo 
+        tipografo prese una cassetta di caratteri e li assemblò per preparare un testo campione. quando.</p>
+    </div>
+    
     <header>
-      <h1 className={`${styles.headerText} text-white text-center  pt-40 pb-10  text-4xl`}>Bli medlem</h1>
+      <h1 className={`${styles.headerText} text-white text-center  pt-20 pb-10  text-4xl`}>Bli medlem</h1>
       
     </header>
-    
-    <form className="flex flex-col items-center border-2 border-indigo-600  w-screen md:w-2/3 m-auto lg:w-2/4 lg:text-lg"  >
+    <form className="flex flex-col items-center  w-screen md:w-2/3 m-auto lg:w-2/4 lg:text-lg"  >
       <div className=" flex justify-center lg:w-[100%] md:w-[100%] sm:w-[100%] ">
         <input className={`${styles.regInputs} w-[41%] `} type="text" id="fname" name="fname" placeholder="Förnamn..."/>
         <input className={`${styles.regInputs} w-[41%] `} type="text" id="fname" name="fname" placeholder="Efternamn..."/>
