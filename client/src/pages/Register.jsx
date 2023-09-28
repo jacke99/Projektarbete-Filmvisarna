cd61c20fb9078b8cab635efd7a4c53982bf7d1f8
marcus nilsson <marcusnilsson2010@gmail.com>
2023-09-28 11:26:17 +0200

    img is now working, next step try to implement header

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,9 @@
 import { styles } from "../styles";
-import chairs from "../assets/chairs.jpg"
+
 
 
 export default function Register() {
-  return <div className="bg-black"  >
+  return <div className= "bg-[url('./assets/chairs.jpg')] bg-cover bg-center " >
     <header>
       <h1 className={`${styles.headerText} text-white text-center  p-10  text-4xl`}>Bli medlem</h1>
       
@@ -15,10 +15,10 @@ export default function Register() {
         <input className={`${styles.regInputs} `} type="text" id="fname" name="fname" placeholder="Efternamn..."/>
       </div>
       
-      <input className={`${styles.regInputs} w-80 `}  type="email" id="email" name="email" placeholder="Epost..."/>
+      <input className={`${styles.regInputs} w-80 `} type="email" id="email" name="email" placeholder="Epost..."/>
       <input className={`${styles.regInputs} w-80`} type="password" id="password" name="password" placeholder="Lösenord..."/>
       <input className={`${styles.regInputs} w-80`} type="password" id="confirm_password" name="confirm_password" placeholder="Bekräfta lösenord..."/>
-      <button className="bg-gold w-40 px-4 rounded py-4 my-5 " >Registrera</button>
+      <button className="bg-gold w-40 px-4 rounded-lg py-4 my-5 " >Registrera</button>
 
     </form >
 
