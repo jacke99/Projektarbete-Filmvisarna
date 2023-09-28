3901bc544f40620995b589a72a41ce57616f5788
Anna Krifors <krifors@hotmail.com>
2023-09-28 21:28:56 +0200

    more-posters

--- client/src/components/BookMovieHero.jsx
+++ client/src/components/BookMovieHero.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import {styles} from "../styles.js";
-import { killersPoster } from "../assets";
+import { killersPoster, pastLivesPoster,smsPoster } from "../assets";
 export default function BookMovieHero(){
     return(
+<>
+<div className="max-w-full p-4 flex items-center justify-between lg:justify-start lg:px-96">
+      <img
+        src={pastLivesPoster}
+        alt="movie poster from Killers of the flower moon"
+        className="w-34 h-48 rounded-lg"
+      />
+      <div className="text-white-100 flex flex-col lg:px-6">
+        <p className="text-xs">Idag 25/9</p>
+        <h2 className="text-base font-extra-bold">The Creator</h2>
+        <p className="font-inconsolata text-xs">Sifi, drama</p>
+        <p className="text-xs">1 tim 33min | 11 år</p>
+      </div>
+      <button className="bg-gold text-black-100 rounded-md px-4 p-1 lg:ml-auto">Boka</button>
+    </div>
+
 
 <div className="max-w-full p-4 flex items-center justify-between lg:justify-start lg:px-96">
       <img
@@ -19,6 +35,21 @@ export default function BookMovieHero(){
       <button className="bg-gold text-black-100 rounded-md px-4 p-1 lg:ml-auto">Boka</button>
     </div>
 
+    <div className="max-w-full p-4 flex items-center justify-between lg:justify-start lg:px-96">
+      <img
+        src={smsPoster}
+        alt="movie poster from Killers of the flower moon"
+        className="w-34 h-48 rounded-lg"
+      />
+      <div className="text-white-100 flex flex-col lg:px-6">
+        <p className="text-xs">Idag 25/9</p>
+        <h2 className="text-base font-extra-bold">The Creator</h2>
+        <p className="font-inconsolata text-xs">Sifi, drama</p>
+        <p className="text-xs">1 tim 33min | 11 år</p>
+      </div>
+      <button className="bg-gold text-black-100 rounded-md px-4 p-1 lg:ml-auto">Boka</button>
+    </div>
+</>
 )
 }
         
