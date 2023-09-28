87f2c94c5750907530d0d48e823e4381e0869f48
marcus nilsson <marcusnilsson2010@gmail.com>
2023-09-28 14:31:41 +0200

    form is now 100% responsive

--- client/src/styles.js
+++ client/src/styles.js
@@ -9,7 +9,7 @@ const styles = {
   trailerTitle: "text-gold sm:text-[20px] md:text-[35px] lg:text-[40px]",
   trailerSubTitle: "text-gold sm:text-[13px] md:text-[20px] lg:text-[25px]",
   regInputs: ": bg-white-500 shadow appearance-none border rounded-lg  py-4 px-3 m-2 my-6 text-gray-700 leading-tight focus:outline-none focus:shadow-outline "
-  
+ 
 };
 
 export { styles };
