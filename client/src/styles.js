313e2b95bcfed85130aae9c203c937cd4bd105ea
Anna Krifors <krifors@hotmail.com>
2023-09-28 13:23:37 +0200

    img-text-fixed

--- client/src/styles.js
+++ client/src/styles.js
@@ -9,7 +9,7 @@ const styles = {
   trailerTitle: "text-gold sm:text-[20px] md:text-[35px] lg:text-[40px]",
   trailerSubTitle: "text-gold sm:text-[13px] md:text-[20px] lg:text-[25px]",
   inputStyle:"w-full border border-gray-300 rounded-md p-1.5 text-sm text-gray-400 my-2 h-8",
-  centerAbsolutePos: "absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+  centerAbsolutePos: "absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2"
 };
 
 export { styles };
