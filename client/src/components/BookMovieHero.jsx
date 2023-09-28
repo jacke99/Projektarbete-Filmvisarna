778cf259793b0156c12909aab4140dd261d06fc4
Anna Krifors <krifors@hotmail.com>
2023-09-28 14:49:31 +0200

    mobile-hero-done

--- client/src/components/BookMovieHero.jsx
+++ client/src/components/BookMovieHero.jsx
@@ -3,19 +3,23 @@ import {styles} from "../styles.js";
 import { killersPoster } from "../assets";
 export default function BookMovieHero(){
     return(
-        <div>
-    <img
-        src={killersPoster} 
+
+<div className="max-w-full p-4 flex items-center justify-between">
+      <img
+        src={killersPoster}
         alt="movie poster from Killers of the flower moon"
-        className={`${styles.moviePosterImg}`}
+        className="w-34 h-48 rounded-lg"
       />
-        <div className="text-white-100">
-        <h2>The Creator</h2>
-        <p>Sifi, drama</p>
-        <p>1 tim 33min | 11 år</p>
-        <p>Idag 25/9</p>
+      <div className="text-white-100 flex flex-col">
+        <p className="text-xs">Idag 25/9</p>
+        <h2 className="text-base font-extra-bold">The Creator</h2>
+        <p className="font-inconsolata text-xs">Sifi, drama</p>
+        <p className="text-xs">1 tim 33min | 11 år</p>
+      </div>
+      <button className="bg-gold text-black-100 rounded-md px-4 p-1">Boka</button>
     </div>
-    <button className="bg-gold">Boka</button>
-        </div>
-    )
+
+)
 }
+        
+  
