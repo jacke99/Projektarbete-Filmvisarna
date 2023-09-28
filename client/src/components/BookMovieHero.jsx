9b1faeb2f9831b03117765356801bef4aaa87477
Anna Krifors <krifors@hotmail.com>
2023-09-28 21:26:26 +0200

    booking-card-done

--- client/src/components/BookMovieHero.jsx
+++ client/src/components/BookMovieHero.jsx
@@ -4,19 +4,19 @@ import { killersPoster } from "../assets";
 export default function BookMovieHero(){
     return(
 
-<div className="max-w-full p-4 flex items-center justify-between">
+<div className="max-w-full p-4 flex items-center justify-between lg:justify-start lg:px-96">
       <img
         src={killersPoster}
         alt="movie poster from Killers of the flower moon"
         className="w-34 h-48 rounded-lg"
       />
-      <div className="text-white-100 flex flex-col">
+      <div className="text-white-100 flex flex-col lg:px-6">
         <p className="text-xs">Idag 25/9</p>
         <h2 className="text-base font-extra-bold">The Creator</h2>
         <p className="font-inconsolata text-xs">Sifi, drama</p>
         <p className="text-xs">1 tim 33min | 11 år</p>
       </div>
-      <button className="bg-gold text-black-100 rounded-md px-4 p-1">Boka</button>
+      <button className="bg-gold text-black-100 rounded-md px-4 p-1 lg:ml-auto">Boka</button>
     </div>
 
 )
