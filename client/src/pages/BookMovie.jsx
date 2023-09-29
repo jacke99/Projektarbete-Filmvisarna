e624f4358cd01ba1929a09af24d350a65d1b8ee3
jacob von scheele <jacobvonscheele@outlook.com>
2023-09-29 14:40:58 +0200

    changed footer import error

--- client/src/pages/BookMovie.jsx
+++ client/src/pages/BookMovie.jsx
@@ -1,14 +1,20 @@
 import ChooseSeats from "../components/ChooseSeats";
 import TicketCounter from "../components/TicketCounter";
-
+import { useStates } from "react-easier";
 
 export default function BookMovie() {
+  //eslint-disable-next-line
+  const counters = useStates("ticketCounter", {
+    adult: 0,
+    child: 0,
+    senior: 0,
+  });
   return (
-    <section>
-        <TicketCounter />
-        <ChooseSeats />
+    <section className="intems-center mt-20 flex flex-col">
+      <TicketCounter />
+      <ChooseSeats />
 
-        <button>Boka</button>
+      <button>Boka</button>
     </section>
-  )
+  );
 }
