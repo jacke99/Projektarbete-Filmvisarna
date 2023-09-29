3117518c05b7e967d14435d5766795fd34c4d1b1
HolaCarmensita <Casandra.c.gustafsson@gmail.com>
2023-09-29 12:04:24 +0200

    tried to make slider and multi-carousel

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,15 +6,16 @@ import {
   theCreatorImage,
   pastLivesImage,
 } from "../assets/index.js";
-
 import MovieImgBlock from "../components/MovieImgBlock.jsx";
 import Carousel from "../components/Carousel.jsx";
+// import MultiCarousel from "../components/multi-carousel.jsx";
 
 export default function Home() {
   return (
     <>
       <Header />
       <main>
+        {/* <MultiCarousel /> */}
         <MovieImgBlock movieImg={killersImage} />
         <Carousel
           moviePoster1={theCreatorPoster}
