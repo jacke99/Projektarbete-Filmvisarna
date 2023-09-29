0dc0b06ee3bd7e3f777df3e1f1126c1204c97a59
jacob von scheele <jacobvonscheele@outlook.com>
2023-09-29 15:52:12 +0200

    created mobile and tablet version of the ticket counter

--- client/src/pages/BookMovie.jsx
+++ client/src/pages/BookMovie.jsx
@@ -1,8 +1,15 @@
 import ChooseSeats from "../components/ChooseSeats";
 import TicketCounter from "../components/TicketCounter";
+import {useStates} from "react-easier"
 
 
 export default function BookMovie() {
+  //eslint-disable-next-line
+  const counters = useStates("ticketCounter", {
+    adult: 0,
+    child: 0,
+    senior: 0,
+  });
   return (
     <section>
         <TicketCounter />
