7c4179af5311ae0f5022e13126fef13a0a6f240e
Anna Krifors <krifors@hotmail.com>
2023-10-02 16:53:33 +0200

    responsive popup

--- client/src/components/ConfirmBooking.jsx
+++ client/src/components/ConfirmBooking.jsx
@@ -10,13 +10,46 @@ export default function ConfirmBooking() {
   };
 
   return (
-    <div className="w-3/4 md:w-1/2 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gold z-50 shadow-md text-center rounded-md">
+
+<div className="w-3/4 md:w-5/12 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gold z-50 shadow-md text-center rounded-md">
   {bookingConfirmed && (
     <div className="w-full flex flex-col h-full relative">
       <img
         src={projector}
         alt="photo of popcorn"
-        className="w-full rounded-t-lg"
+        className="w-full rounded-t-lg md:h-60"
+      />
+      <h1 className={`${styles.centerAbsolutePos} top-1/4 text-white-100 text-3xl font-bold`}>
+        Tack för din bokning!
+      </h1>
+      <div className="flex-grow p-5 flex flex-col justify-between">
+        <p><span className="font-inconsolata">Bokningsnummer:</span> X2354</p>
+        <p><span className="font-inconsolata">Film:</span> Past lives</p>
+        <p>Biljetter: 1 Vuxen, 3 Barn</p>
+        <p>Plats: Rad 4, stol 17,18,19,20</p>
+        <p>datum: 2024-03-22</p>
+        <p>Epost: dinmail@mail.com</p>
+        <p>Tel: 07633XXXXX</p>
+        Pris: 670 SEK
+        <button onClick={handleCloseConfirmation} className='booking-btn mt-4 text-white-100 py-1 px-2.5 rounded-sm bg-black-100 md:w-1/2 mx-auto'>
+          Stäng
+        </button>
+      </div>
+    </div>
+  )}
+</div>
+
+
+  );
+}
+
+{/*     <div className="w-3/4 md:w-5/12 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gold z-50 shadow-md text-center rounded-md">
+  {bookingConfirmed && (
+    <div className="w-full flex flex-col h-full relative">
+      <img
+        src={projector}
+        alt="photo of popcorn"
+        className="w-full rounded-t-lg md:h-60"
       />
       <h1 className={`${styles.centerAbsolutePos} top-1/4 text-white-100 text-3xl font-bold`}>
         Tack för din bokning!
@@ -30,40 +63,10 @@ export default function ConfirmBooking() {
         <p>Epost: dinmail@mail.com</p>
         <p>Tel: 07633XXXXX</p>
         Pris: 670 SEK
-        <button onClick={handleCloseConfirmation} className='booking-btn mt-4 text-white-100 py-1 px-2.5 rounded-sm bg-black-100'>
+        <button onClick={handleCloseConfirmation} className='booking-btn mt-4 text-white-100 py-1 px-2.5 rounded-sm bg-black-100 md:w-1/2'>
           Stäng
         </button>
       </div>
     </div>
   )}
-</div>
-
-
-  );
-}
-
-{/*    <div className="w-3/4 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gold z-50 shadow-md text-center rounded-md">
-      {bookingConfirmed && (
-        <div className="w-full relative">
-        <img
-        src={projector}
-        alt="photo of popcorn"
-        className="w-full rounded-t-lg"
-      />
-      
-            
-          <h1 className={`${styles.centerAbsolutePos} top-1/4  text-white-100 text-3xl font-bold`}>Tack för din bokning!</h1>
-          <div className="p-2">
-          <p>Bokningsnummer: X2354</p>
-          <p>Film: Past lives</p>
-          <p>Biljetter: 1 Vuxen, 3 Barn</p>
-          <p>Plats: Rad 4, stol 17,18,19,20</p>
-          <p>datum: 2024-03-22</p>
-          <p>Epost: dinmail@mail.com</p>
-          <p>Tel: 07633XXXXX</p>
-          Pris: 670 SEK
-          <button onClick={handleCloseConfirmation} className='booking-btn text-white-100 py-1 px-2.5 rounded-sm bg-black-100'>Stäng</button>
-          </div>
-        </div>
-      )}
-      </div>*/}
+</div>   */}
