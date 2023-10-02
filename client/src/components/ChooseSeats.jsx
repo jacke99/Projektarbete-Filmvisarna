fcd87504e95bbdbed6f0675d3c8f4ec9f7474846
felixlindh <113009702+felixlindh@users.noreply.github.com>
2023-10-02 16:05:34 +0200

    added a hover color  when choosing your seat and a bg-color for selected seats

--- client/src/components/ChooseSeats.jsx
+++ client/src/components/ChooseSeats.jsx
@@ -1,7 +1,14 @@
-
+import {useState, useEffect} from "react"
 export default function ChooseSeats() {
+    const [seats, setSeats] = useState([])
+    
+    useEffect(() => {
+      seats.forEach(seat => {
+        document.getElementById(`row${seat.row}seat-${seat.seat}`).classList.add("bg-white")
+      });
+    }, [seats])
 
-    function bookSeats(event, numberOfSeats) {
+    function handleMouseEnter(event, numberOfSeats) {
         const target = event.target 
         const parent = event.target.parentElement
         // eslint-disable-next-line
@@ -9,25 +16,74 @@ export default function ChooseSeats() {
         // eslint-disable-next-line
         const [prefix2, row] = parent.id.split('-');
         const seatIndex = parseInt(seatId, 10);
-        const rowId = parseInt(row, 10);
+        const rowIndex = parseInt(row, 10);
       
-        const startSeatIndex = Math.max(0, seatIndex - Math.floor(numberOfSeats / 2));
+        const startSeatIndex = Math.max(0, seatIndex - numberOfSeats);
+        const endSeatIndex = Math.min(startSeatIndex + numberOfSeats - 1, 11);
+      
+        const hoveringSeats = [];
+      
+        for (let i = startSeatIndex; i <= endSeatIndex; i++) {
+          hoveringSeats.push({ row: rowIndex, seat: i + 1});
+        }
+        hoveringSeats.forEach(seat => {
+          document.getElementById(`row${seat.row}seat-${seat.seat}`).classList.add("bg-gold")
+        });
+        
+    }
+
+    function handleMouseLeave(event, numberOfSeats) {
+      const target = event.target 
+        const parent = event.target.parentElement
+        // eslint-disable-next-line
+        const [prefix, seatId] = target.id.split('-');
+        // eslint-disable-next-line
+        const [prefix2, row] = parent.id.split('-');
+        const seatIndex = parseInt(seatId, 10);
+        const rowIndex = parseInt(row, 10);
+      
+        const startSeatIndex = Math.max(0, seatIndex - numberOfSeats);
+        const endSeatIndex = Math.min(startSeatIndex + numberOfSeats - 1, 11);
+      
+        const hoveringSeats = [];
+      
+        for (let i = startSeatIndex; i <= endSeatIndex; i++) {
+          hoveringSeats.push({ row: rowIndex, seat: i + 1});
+        }
+        hoveringSeats.forEach(seat => {
+          document.getElementById(`row${seat.row}seat-${seat.seat}`).classList.remove("bg-gold")
+        });
+    }
+    
+    function bookSeats(event, numberOfSeats) {
+        
+        const target = event.target 
+        const parent = event.target.parentElement
+        // eslint-disable-next-line
+        const [prefix, seatId] = target.id.split('-');
+        // eslint-disable-next-line
+        const [prefix2, row] = parent.id.split('-');
+        const seatIndex = parseInt(seatId, 10);
+        const rowIndex = parseInt(row, 10);
+      
+        const startSeatIndex = Math.max(0, seatIndex - numberOfSeats);
         const endSeatIndex = Math.min(startSeatIndex + numberOfSeats - 1, 11);
       
         const selectedSeats = [];
       
         for (let i = startSeatIndex; i <= endSeatIndex; i++) {
-          selectedSeats.push({ row: rowId, seat: i + 1});
+          selectedSeats.push({ row: rowIndex, seat: i + 1});
         }
-       
-        console.table(selectedSeats);
-        console.log(target.id)
+        setSeats(selectedSeats)
+        
     }
 
     // eslint-disable-next-line
     const Seat = ({ seatNumber, rowNumber }) => (
         <div className={"bg-footerGrey seat lg:w-5 lg:h-5 md:w-8 md:h-8 w-5 h-5 cursor-pointer"} 
         key={seatNumber} id={`row${rowNumber}seat-${seatNumber}`} onClick={(event) => bookSeats(event, 4)}
+        onMouseEnter={(event => handleMouseEnter(event, 4))}
+        onMouseLeave={(event) => handleMouseLeave(event, 4)}
         >
         </div>
       );
