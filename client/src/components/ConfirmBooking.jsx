1156c3de85eb057b76fced5b8b1c8c23b681dd5e
Anna Krifors <krifors@hotmail.com>
2023-10-02 08:04:42 +0200

    more style on popup

--- client/src/components/ConfirmBooking.jsx
+++ client/src/components/ConfirmBooking.jsx
@@ -8,14 +8,14 @@ export default function ConfirmBooking() {
   };
 
   return (
-    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gold z-50 p-5 shadow-md text-center">
+    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gold z-50 p-5 shadow-md text-center rounded-md">
       {bookingConfirmed && (
         <div className="booking-confirmation">
           <h1>Tack för din bokning!</h1>
           <p>dina bokade platser: stol 33, stol 32 rad 3</p>
           <p>dinmail@mail.com</p>
           <p>07633XXXXX</p>
-          <button onClick={handleCloseConfirmation} className='booking-btn'>Close</button>
+          <button onClick={handleCloseConfirmation} className='booking-btn text-white-100 py-1 px-2.5 rounded-sm bg-black-100'>Stäng</button>
         </div>
       )}
     </div>
