db37d952ab14c3864829e11b86d0d628f72b2a83
felixlindh <113009702+felixlindh@users.noreply.github.com>
2023-10-02 10:08:38 +0200

    updated row and seat id's to be unique

--- client/src/components/ChooseSeats.jsx
+++ client/src/components/ChooseSeats.jsx
@@ -1,13 +1,15 @@
 
-
 export default function ChooseSeats() {
 
     function bookSeats(event, numberOfSeats) {
         const target = event.target 
         const parent = event.target.parentElement
-        
-        const seatIndex = parseInt(target.id, 10);
-        const rowId = parseInt(parent.id, 10);
+        // eslint-disable-next-line
+        const [prefix, seatId] = target.id.split('-');
+        // eslint-disable-next-line
+        const [prefix2, row] = parent.id.split('-');
+        const seatIndex = parseInt(seatId, 10);
+        const rowId = parseInt(row, 10);
       
         const startSeatIndex = Math.max(0, seatIndex - Math.floor(numberOfSeats / 2));
         const endSeatIndex = Math.min(startSeatIndex + numberOfSeats - 1, 11);
@@ -17,26 +19,26 @@ export default function ChooseSeats() {
         for (let i = startSeatIndex; i <= endSeatIndex; i++) {
           selectedSeats.push({ row: rowId, seat: i + 1});
         }
-      
+       
         console.table(selectedSeats);
         console.log(target.id)
     }
 
     // eslint-disable-next-line
-    const Seat = ({ seatNumber }) => (
+    const Seat = ({ seatNumber, rowNumber }) => (
         <div className={"bg-footerGrey seat lg:w-5 lg:h-5 md:w-8 md:h-8 w-5 h-5 cursor-pointer"} 
-        key={seatNumber} id={seatNumber + 1} onClick={(event) => bookSeats(event, 4)}
+        key={seatNumber} id={`row${rowNumber}seat-${seatNumber}`} onClick={(event) => bookSeats(event, 4)}
         >
         </div>
       );
     // eslint-disable-next-line
     const Row = ({ rowNumber }) => {
     const seats = Array.from({ length: 12 }, (_, index) => (
-        <Seat key={index} seatNumber={index} />
+        <Seat key={index} seatNumber={index +1} rowNumber={rowNumber}/>
     ));
       
     return (
-        <div className="flex justify-between w-full" key={rowNumber} id={rowNumber + 1}>
+        <div className="flex justify-between w-full" key={rowNumber} id={`row-${rowNumber}`}>
         {seats}
         </div>
     );
@@ -44,7 +46,7 @@ export default function ChooseSeats() {
       
     const DivGenerator = () => {
     const rows = Array.from({ length: 8 }, (_, index) => (
-        <Row key={index} rowNumber={index} />
+        <Row key={index} rowNumber={index + 1} />
     ));
     
     return <div>{rows}</div>;
