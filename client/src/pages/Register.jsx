4fef14c1613d526992b915c37b2258e16a4539f3
marcus nilsson <marcusnilsson2010@gmail.com>
2023-10-03 09:15:54 +0200

    Register component is 98% complete. I've tidied up the code quite a bit. What I haven't done, however, is change so we use React-easier from hook/formValidator.js to Register.jsx. Ask someone for help during the day

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,9 +2,8 @@ import { styles } from "../styles";
 import { useFormDefaults } from '../hooks/formValidation'
 
 
-
-
 export default function Register() {
+ 
   let { defaults, formData } = useFormDefaults();
 
   return <div className= "bg-[url('./assets/chairs.jpg')] bg-cover bg-center " >
@@ -22,31 +21,45 @@ export default function Register() {
       <h1 className={`${styles.headerText} text-white text-center  pt-20 pb-10  text-4xl`}>Bli medlem</h1>
     </header>
 
-    {/* Firstname and lastname */}
     <form className="flex flex-col items-center  w-screen md:w-2/3 m-auto lg:w-2/4 lg:text-lg"  >
+    {/* Firstname and lastname */}
       <div className=" flex justify-center lg:w-[100%] md:w-[100%] sm:w-[100%] ">
-        <input className={`${styles.regInputs} w-[41%] `} type="text" {...defaults('Fname', 'Förnamn. . .')} />
-        <input className={`${styles.regInputs} w-[41%] `} type="text" {...defaults('Lname', 'Efternamn. . .')} />
+        <input 
+           className={`${styles.regInputs} w-[41%] `} 
+           type="text" {...defaults('Fname', 'Förnamn. . .')} />
+        
+        <input 
+          className={`${styles.regInputs} w-[41%] `} 
+          type="text" {...defaults('Lname', 'Efternamn. . .')} />
       </div>
       
       {/* Phone number */}
-      <input className={`${styles.regInputs} w-[85%]  `}  type="phone" {...defaults('phone', 'Telefon. . . ', { minLength: 8, type: 'tel' }, 
+      <input 
+        className={`${styles.regInputs} w-[85%]  `}  
+        type="phone" {...defaults('phone', 'Telefon. . . ', { minLength: 8, type: 'tel' }, 
         val => /^\d*$/.test(val), 'OBS - Endast nummer är tillåten på denna rad!')} />
       
       {/* Email address */}
       <input className={`${styles.regInputs} w-[85%]  `} {...defaults('email', 'Epost. . .')}/>
       
       {/* Password */}
-      <input className={`${styles.regInputs} w-[85%] `} {...defaults('password', 'Lösenord. . .', { minLength: 8, type: 'password' }, val => /\d/.test(val) && /[A-Z]/.test(val) && /[a-z]/.test(val),
-      'OBS - Lösenordet måste innehålla : 1 stor bokstav, en liten bokstav samt minst 1 siffra' )} /> 
+      <input 
+        className={`${styles.regInputs} w-[85%] `} {...defaults('password', 'Lösenord. . .', { minLength: 8, 
+        type: 'password' }, 
+        val => /\d/.test(val) && /[A-Z]/.test(val) && /[a-z]/.test(val),
+        'OBS - Lösenordet måste innehålla : 1 stor bokstav, en liten bokstav samt minst 1 siffra' )} /> 
       
       {/* Password Confirm */}
-      <input className={`${styles.regInputs} w-[85%]  `} {...defaults('confirm_password', 'Bekräfta lösenord. . .',  { minLength: 8, type: 'password' }, 
-      val => val === formData.password, 'OBS - lösenorden måste matcha varandra!'
+      <input 
+        className={`${styles.regInputs} w-[85%]  `} {...defaults('confirm_password', 'Bekräfta lösenord. . .',  { minLength: 8, 
+        type: 'password' }, 
+        val => val === formData.password, 'OBS - lösenorden måste matcha varandra!'
       )} />
 
       {/* Button Submit */}
-      <button className="bg-gold w-40 px-4 rounded-lg py-4 my-5 lg:text-lg " type="submit" >Registrera</button>
+      <button 
+        className="bg-gold w-40 px-4 rounded-lg py-4 my-5 lg:text-lg " 
+        type="submit" >Registrera</button>
 
     </form >
 
