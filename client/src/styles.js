95eb3cef8233b2010aa4713c07cbbea44db0e672
felixlindh <113009702+felixlindh@users.noreply.github.com>
2023-10-03 15:16:31 +0200

    choosen seats is now displayed on screen for the user to see, also changed some styles

--- client/src/styles.js
+++ client/src/styles.js
@@ -3,7 +3,7 @@ const styles = {
   paddingY: "sm:py-16 py-6",
   padding: "sm:px-16 px-6 sm:py-16 py-10",
   headerText: "text-gold sm: text-[20px] md:text-[45px] lg:text-[50px]",
-  subHeaderText: "text-gold sm:text-[16px] md:text-[25px] lg:text-[30px]",
+  subHeaderText: "text-gold sm:text-[16px] md:text-[25px] lg:text-[27px]",
   movieTitle:
     "text-white-100 font-inconsolata  sm:text-[15px] md:text-[20px] lg:text-[25px]",
   trailerTitle: "text-gold sm:text-[20px] md:text-[30px] lg:text-[35px]",
