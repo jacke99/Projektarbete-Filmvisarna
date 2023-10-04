b40d52a1ff102579ae9d8c4c907377b61e1746af
Anna Krifors <krifors@hotmail.com>
2023-10-04 14:48:45 +0200

    fixed pop up css
    co-author-by: HolaCarmensita

--- client/src/components/ConfirmBooking.jsx
+++ client/src/components/ConfirmBooking.jsx
@@ -6,7 +6,39 @@ import { styles } from "../styles";
 export default function ConfirmBooking() {
   const navigate = useNavigate()
   return (
-<div className="w-3/4 md:w-5/12 lg:w-96 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gold z-50 shadow-md text-center rounded-md">
+    <>
+<div className="bg-white-100 w-3/4 md:w-5/12 lg:w-96 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 rounded-md">
+  
+  <div className="text-center mt-4 w-full h-full">
+  
+    <h1 className={` top-1/4 text-xl font-bold`}>
+      Tack för din bokning!
+    </h1>
+    <div className="flex-grow p-5 flex flex-col items-start justify-between">
+      <p className="text-left mb-2">Bokningsbekräftelse har nu skickats till din email!</p>
+      <p>Bokningsnummer: <span>X2354</span> </p>
+      <p>Film: <span>Past lives</span></p>
+      <p>Biljetter: <span> 1 Vuxen, 3 Barn</span></p>
+      <p>Plats: <span> Rad 4, stol 17,18,19,20</span></p>
+      <p>datum: <span> 2024-03-22</span></p>
+      <p>Epost: <span>dinmail@mail.com</span></p>
+      <p>Tel: <span>0763399987</span></p>
+      <p>Pris: <span>670 SEK</span></p>
+     
+    </div>
+    <button onClick={() => navigate("/")} className='bg-gold text-black-100 rounded-md px-4 p-1 mb-4'>
+        Stäng
+      </button>
+  </div>
+
+ </div>
+
+</>
+
+  );
+}
+
+{/*<div className="w-3/4 md:w-5/12 lg:w-96 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-primary z-50 shadow-md text-center rounded-md">
   
     <div className="w-full flex flex-col h-full relative">
       <img
@@ -17,24 +49,20 @@ export default function ConfirmBooking() {
       <h1 className={`${styles.centerAbsolutePos} top-1/4  text-white-100 text-3xl font-bold`}>
         Tack för din bokning!
       </h1>
-      <div className="flex-grow p-5 flex flex-col justify-between">
-        <p className="font-xs font-inconsolata">Bokningsnummer: <span>X2354</span> </p>
-        <p className="font-xs font-inconsolata">Film: <span>Past lives</span></p>
-        <p className="font-xs font-inconsolata">Biljetter: <span> 1 Vuxen, 3 Barn</span></p>
-        <p className="font-xs font-inconsolata">Plats: <span> Rad 4, stol 17,18,19,20</span></p>
-        <p className="font-xs font-inconsolata">datum: <span> 2024-03-22</span></p>
-        <p className="font-xs font-inconsolata">Epost: <span>dinmail@mail.com</span></p>
-        <p className="font-xs font-inconsolata">Tel: <span>0763399987</span></p>
-        <p className="font-xs font-inconsolata">Pris: <span>670 SEK</span></p>
-        <button onClick={() => navigate("/")} className='booking-btn mt-4 text-white-100 py-1 px-2.5 rounded bg-black-100 md:w-1/2 mx-auto'>
+      <div className="flex-grow p-5 flex flex-col justify-between text-white-100">
+        <p>Bokningsbekräftelse har nu skickats till din email</p>
+        <p className="font-bold">Bokningsnummer: <span>X2354</span> </p>
+        <p className="font-xs">Film: <span>Past lives</span></p>
+        <p className="font-xs">Biljetter: <span> 1 Vuxen, 3 Barn</span></p>
+        <p className="font-xs">Plats: <span> Rad 4, stol 17,18,19,20</span></p>
+        <p className="font-xs">datum: <span> 2024-03-22</span></p>
+        <p className="font-xs">Epost: <span>dinmail@mail.com</span></p>
+        <p className="font-xs">Tel: <span>0763399987</span></p>
+        <p className="font-xs">Pris: <span>670 SEK</span></p>
+        <button onClick={() => navigate("/")} className='booking-btn mt-4 py-1 pz-0.5 text-black rounded bg-gold md:w-1/2 mx-auto'>
           Stäng
         </button>
       </div>
     </div>
   
-</div>
-
-
-  );
-}
-
+</div>*/}
