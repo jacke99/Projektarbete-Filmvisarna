6d9f3dd5c74fc27d0e0dedff9002cf94a9a278f2
HolaCarmensita <Casandra.c.gustafsson@gmail.com>
2023-10-04 16:12:03 +0200

    Ipad och desktop utan knappar responsiva hinner inte

--- client/src/components/MovieCardForFilmer.jsx
+++ client/src/components/MovieCardForFilmer.jsx
@@ -6,18 +6,18 @@ export default function MovieCardForFilmer() {
   const navigate = useNavigate();
 
   return (
-    <>
-      <div className="flex flex-col justify-between border-t-[0.5px] border-gold p-6  lg:justify-start lg:px-96">
+    <div className="lg:flex lg:justify-center">
+      <div className=" lg:pr-15 flex max-w-7xl flex-col justify-between border-t-[0.5px] border-gold py-8  md:flex-row md:gap-4 md:pr-6 lg:flex-row lg:justify-start lg:gap-8">
         <img
           src={killersImage}
           alt="movie poster from Killers of the flower moon"
-          className="h-[250px] w-full object-cover object-center lg:h-[550px]"
+          className="h-[250px]  w-full object-cover object-center lg:h-[450px] lg:w-1/2"
         />
-        <div>
+        <div className="md: flex flex-col justify-center">
           <div className="my-4 flex flex-col text-white-100">
             <h2 className="font-extra-bold text-base">The Creator</h2>
             <p className="font-inconsolata text-xs">Sifi, drama</p>
-            <p className="flex-col pt-1 text-xs md:w-1/2">
+            <p className="flex-col pt-1 text-xs">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam
               facere totam tenetur omnis consequuntur vero ipsum, reprehenderit
               molestiae a quae illum esse quo nulla blanditiis voluptatibus amet
@@ -26,12 +26,12 @@ export default function MovieCardForFilmer() {
           </div>
           <button
             onClick={() => navigate("/movies/1")}
-            className="rounded-md bg-gold p-1 px-4 text-black-100"
+            className="self-start rounded-md bg-gold p-1 px-4 text-black-100"
           >
             Biljetter
           </button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
