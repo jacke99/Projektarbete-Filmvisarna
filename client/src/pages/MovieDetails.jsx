73dc629e5c9599bfe0a7a922f97a3ebd439fa3f0
jacob von scheele <jacobvonscheele@outlook.com>
2023-10-04 16:52:12 +0200

    Fixed css styling details for booking and movieDetails
    Co-authored-by: BosMar-Dev
    Co-authored-by: Hipnosic

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -16,7 +16,7 @@ export default function MovieDetails() {
   }, [])
   return (
     <div className=" mt-20 mb-20 h-full bg-primary">
-      <div className="relative h-96 md:h-[30rem] lg:h-[36rem]">
+      <div className="relative h-96 md:h-[30rem] lg:h-[36rem] ">
         <img
           src={killersImage}
           alt="movie poster"
@@ -37,8 +37,8 @@ export default function MovieDetails() {
           </p>
         </div>
       </div>
-      <div className="relative m-auto flex flex-col p-8 sm:p-12 md:w-5/6 lg:w-2/3 lg:pb-8 xl:w-3/5 2xl:w-3/6">
-        <p className={` mb-8 text-white sm:text-xl `}>
+      <div className="relative m-auto flex flex-col p-8 sm:p-12 lg:pb-8">
+        <p className={` mb-4 text-white sm:text-xl md:w-5/6 lg:w-2/3 xl:w-3/5 2xl:w-3/6 md:m-auto`}>
           Lorem Ipsum è un testo segnaposto utilizzato nel settore della
           tipografia e della stampa. Lorem Ipsum è considerato il testo
           segnaposto standard sin dal sedicesimo secolo, quando un anonimo
@@ -46,33 +46,33 @@ export default function MovieDetails() {
           un testo campione. quando un anonimo tipografo prese una cassetta di
           caratteri e li assemblò per preparare un.
         </p>
-        <button onClick={handleClickScroll} className=" m-auto mb-4 rounded-xl bg-gold px-4 py-2 text-xl text-black sm:px-5 sm:text-2xl">
+        <button onClick={handleClickScroll} className="mb-4 mt-2 md:mt-6 md:mb-6 lg:mt-8 lg:mb-8 rounded-xl bg-gold px-4 py-2 text-xl text-black sm:px-5 sm:text-2xl mx-auto">
           Biljetter
         </button>
-        <div className={`${styles.movieDescInfo}`}>
+        <div className={`${styles.movieDescInfo} md:w-5/6 lg:w-2/3 xl:w-3/5 2xl:w-3/6 md:m-auto`}>
           <span>Regi:</span>
           <span>Lorem Ipsum</span>
         </div>
-        <div className={`${styles.movieDescInfo}`}>
+        <div className={`${styles.movieDescInfo} md:w-5/6 lg:w-2/3 xl:w-3/5 2xl:w-3/6 md:m-auto`}>
           <span>Skådespelare:</span>
           <span>Testo segnaposto, Testo segnaposto</span>
         </div>
-        <div className={`${styles.movieDescInfo}`}>
+        <div className={`${styles.movieDescInfo} md:w-5/6 lg:w-2/3 xl:w-3/5 2xl:w-3/6 md:m-auto`}>
           <span>Originaltitel:</span>
           <span>testo segnaposto</span>
         </div>
-        <div className={`${styles.movieDescInfo}`}>
+        <div className={`${styles.movieDescInfo} md:w-5/6 lg:w-2/3 xl:w-3/5 2xl:w-3/6 md:m-auto`}>
           <span>Premiär:</span>
           <span>22 sep 2023</span>
         </div>
-        <div className={`${styles.movieDescInfo}`}>
+        <div className={`${styles.movieDescInfo} md:w-5/6 lg:w-2/3 xl:w-3/5 2xl:w-3/6 md:m-auto`}>
           <span>Originalspråk</span>
           <span>Lorem Ipsum</span>
         </div>
         <img
           src={killersPoster}
           alt="movie poster"
-          className=" absolute top-[24rem] right-12 hidden h-56 sm:block lg:h-72 lg:top[22rem]"
+          className=" absolute top-[22rem] right-[6rem] hidden h-56 sm:block lg:h-72 lg:top[22rem] lg:right-[19rem]"
         />
         <p id="scrollTo"></p>
         <MovieFilterForm />
