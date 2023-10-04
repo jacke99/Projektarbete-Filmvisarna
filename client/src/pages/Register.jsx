9b10468b42e04baa0fbc7a0f16a034abb3ac2b97
marcus nilsson <marcusnilsson2010@gmail.com>
2023-10-04 13:24:37 +0200

    cleaning code registration page with Jacob and Daniel. Registration is now OK

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -12,7 +12,7 @@ export default function Register() {
       <h2 className={`${styles.headerText} text-white text-center  pt-40 pb-10  text-4xl`}>
         Bli en del av familjen!
       </h2>
-      <p className=" text-white text-center sm:w-[70%] px-6 md:-[60%] m-auto lg:text-lg">Välkommen till oss på Filmvisarna. Vi älskar film! Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia
+      <p className=" text-white text-center sm:w-[70%] px-6 md:-[60%] m-auto lg:text-lg max-w-[60rem]">Välkommen till oss på Filmvisarna. Vi älskar film! Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia
          e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, quando un anonimo 
         tipografo prese una cassetta di caratteri e li assemblò per preparare un testo campione. quando.</p>
     </div>
@@ -21,17 +21,17 @@ export default function Register() {
       <h1 className={`${styles.headerText} text-white text-center  pt-20 pb-10  text-4xl`}>Bli medlem</h1>
     </header>
 
-    <form className="flex flex-col items-center  w-screen md:w-2/3 m-auto lg:w-2/4 lg:text-lg"  >
+    <form className="flex flex-col items-center  w-screen md:w-2/3 m-auto lg:w-2/4 lg:text-lg max-w-[50rem] "  >
 
     {/* Firstname and lastname */}
-      <div className=" flex justify-center lg:w-[100%] md:w-[100%] sm:w-[100%] ">
+      <div className=" flex items-center flex-col lg:w-[100%] md:w-[100%] sm:w-[100%] w-screen md:flex-row md:justify-center ">
         <input 
-           className={`${styles.regInputs} w-[41%] `} 
+           className={`${styles.regInputs} w-[85%] md:w-[41%] `} 
            type="text" 
            {...defaults('Fname', 'Förnamn. . .')} />
         
         <input 
-          className={`${styles.regInputs} w-[41%] `} 
+          className={`${styles.regInputs} w-[85%] md:w-[41%] `} 
           type="text" 
           {...defaults('Lname', 'Efternamn. . .')} />
       </div>
@@ -66,7 +66,7 @@ export default function Register() {
 
       {/* Button Submit */}
       <button 
-        className="bg-gold w-40 px-4 rounded-lg py-4 my-5 lg:text-lg " 
+        className="bg-gold w-40 px-4 rounded-lg py-4 my-5 mb-10 lg:text-lg " 
         type="submit" >Registrera</button>
 
     </form >
