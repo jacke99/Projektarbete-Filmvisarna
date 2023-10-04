4fb85d8ca19fe884bc926fc0e0c3bb3f9ed2f6b4
HolaCarmensita <Casandra.c.gustafsson@gmail.com>
2023-10-04 17:47:08 +0200

    Text and button resposive

--- client/src/components/MovieCardForFilmer.jsx
+++ client/src/components/MovieCardForFilmer.jsx
@@ -13,11 +13,13 @@ export default function MovieCardForFilmer() {
           alt="movie poster from Killers of the flower moon"
           className="h-[250px]  w-full object-cover object-center lg:h-[450px] lg:w-1/2"
         />
-        <div className="md: flex flex-col justify-center">
-          <div className="my-4 flex flex-col text-white-100">
-            <h2 className="font-extra-bold text-base">The Creator</h2>
-            <p className="font-inconsolata text-xs">Sifi, drama</p>
-            <p className="flex-col pt-1 text-xs">
+        <div className="md: flex flex-col justify-center md:justify-around lg:justify-around">
+          <div className="my-4 flex flex-col text-white-100 md:my-0 lg:my-0">
+            <h2 className="font-extra-bold text-base md:text-lg lg:text-xl">
+              The Creator
+            </h2>
+            <p className="font-inconsolata text-sm lg:text-base">Sifi, drama</p>
+            <p className="flex-col pt-1 text-sm lg:text-lg">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam
               facere totam tenetur omnis consequuntur vero ipsum, reprehenderit
               molestiae a quae illum esse quo nulla blanditiis voluptatibus amet
@@ -26,7 +28,7 @@ export default function MovieCardForFilmer() {
           </div>
           <button
             onClick={() => navigate("/movies/1")}
-            className="self-start rounded-md bg-gold p-1 px-4 text-black-100"
+            className="self-start rounded-md bg-gold px-4 py-2 text-black-100 lg:px-6 lg:py-3 lg:text-lg"
           >
             Biljetter
           </button>
