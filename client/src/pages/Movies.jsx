0b6b305e92f66810b0dd92305537bd6c3589ad9a
HolaCarmensita <Casandra.c.gustafsson@gmail.com>
2023-10-04 17:47:08 +0200

    Text and button resposive

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -3,8 +3,21 @@ import MovieCardForFilmer from "../components/MovieCardForFilmer";
 export default function Movies() {
   return (
     <div className="px-8">
-      <div className="m-6 mt-28 text-center text-2xl text-white">
-        <h1>Våra Filmer</h1>
+      <div
+        className="
+      
+      mb-6 mt-20 
+      
+      text-center text-2xl
+      
+      
+       text-white 
+       
+       md:mb-12 md:mt-32 lg:mb-20 lg:mt-40"
+      >
+        <h1 className="font-extra-bold text-xl md:text-2xl lg:text-3xl">
+          Våra Filmer
+        </h1>
       </div>
       <MovieCardForFilmer />
       <MovieCardForFilmer />
