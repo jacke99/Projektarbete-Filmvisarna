64d108d0b3caca793967eb843383ea67f57be1c7
jacob von scheele <jacobvonscheele@outlook.com>
2023-10-05 08:13:19 +0200

    added navigation to home page carousel cards

--- client/src/components/movieCard.jsx
+++ client/src/components/movieCard.jsx
@@ -1,13 +1,17 @@
 /* eslint-disable react/prop-types */
 
+import { useNavigate } from "react-router-dom";
+
 //MovieCars används som kort för filmer och tillhörande filmtitel i bildkarusellen
 export default function MovieCard({ title, img, alt }) {
+  const navigate = useNavigate()
   return (
     <div className="flex flex-col items-center justify-center gap-3">
       <img
         className="sm: h-[222px] w-[150px] rounded-lg object-cover object-center md:h-[252px] md:w-[170px] lg:h-[296px] lg:w-[200px]"
         src={img}
         alt={alt}
+        onClick={() => navigate("/movies/1")}
       />
 
       <h2
