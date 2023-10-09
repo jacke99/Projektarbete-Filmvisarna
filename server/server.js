832a6a2477a3bddda59437dadcbb2bc84e41836c
jacob von scheele <jacobvonscheele@outlook.com>
2023-10-09 14:22:45 +0200

    Initial backend setup

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import router from "./src/router/router.js";
+
+const addr = "127.0.0.1";
+const port = 3030;
+const app = express();
+
+app.use(express.json());
+app.use("/api", router);
+
+app.get("/health", (req, res) => {
+    res.send("Server is up and healthy");
+})
+
+app.listen(port, addr, () => {
+    console.log(`Server is listening on http://${addr}:${port}`);
+})
