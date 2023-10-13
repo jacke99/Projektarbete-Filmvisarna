68aee5a9f00c75107e3b47931731834d490ae8da
Daniel Rotaru <113009669+Hipnosic@users.noreply.github.com>
2023-10-13 16:19:18 +0200

    Added css to admin page.
    Co-Authored-By:BosMar-Dev.

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,61 +1,130 @@
 import React, { useState } from "react";
+import { styles } from "../styles.js";
 
 function Admin() {
     const [showMoviesForm, setShowMoviesForm] = useState(false);
+    const [showBookingsForm, setShowBookingsForm] = useState(false);
+    const [showUsersForm, setShowUsersForm] = useState(false);
+    const [showTheatersForm, setShowTheatersForm] = useState(false);
+    const [showScreeningsForm, setShowScreeningsForm] = useState(false);
 
     const toggleMoviesForm = () => {
         setShowMoviesForm(!showMoviesForm);
     };
 
+    const toggleBookingsForm = () => {
+        setShowBookingsForm(!showBookingsForm);
+    };
+
+    const toggleUsersForm = () => {
+        setShowUsersForm(!showUsersForm);
+    };
+
+    const toggleTheatersForm = () => {
+        setShowTheatersForm(!showTheatersForm);
+    };
+
+    const toggleScreeningsForm = () => {
+        setShowScreeningsForm(!showScreeningsForm);
+    };
+
     return (
-        <div className="min-h-screen bg-gray-100">
-            <div className="py-6">
+        <div className="min-h-screen bg-primary">
+            <div className="py-6 mt-10 pl-4">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <h1 className="text-2xl font-semibold text-gray-900">Admin Dashboard</h1>
+                    <h1 className="text-2xl font-semibold text-white">Admin Dashboard</h1>
                 </div>
             </div>
             <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4">
-                    <div className="bg-white overflow-hidden shadow rounded-lg">
+                <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-4">
+                    <div className="bg-black overflow-hidden shadow rounded-lg border-2 border-gold">
                         <div className="px-4 py-5 sm:p-6">
-                            <h3 className="text-lg font-medium text-gray-900">Bookings</h3>
-                            {/* Add your content for Bookings here */}
-                        </div>
-                    </div>
-                    <div className="bg-white overflow-hidden shadow rounded-lg">
-                        <div className="px-4 py-5 sm:p-6">
-                            <h3 className="text-lg font-medium text-gray-900">Users</h3>
-                            {/* Add your content for Users here */}
-                        </div>
-                    </div>
-                    <div className="bg-white overflow-hidden shadow rounded-lg">
-                        <div className="px-4 py-5 sm:p-6">
-                            <h3 className="text-lg font-medium text-gray-900">Screenings</h3>
-                            {/* Add your content for Screenings here */}
-                        </div>
-                    </div>
-                    <div className="bg-white overflow-hidden shadow rounded-lg">
-                        <div className="px-4 py-5 sm:p-6">
-                            <h3 className="text-lg font-medium text-gray-900">Movies</h3>
+                            <h3 className="text-lg font-medium text-white">Bookings</h3>
                             <button
-                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                                className="mb-4 mt-2 md:mt-6 md:mb-6 lg:mt-8 lg:mb-8 rounded-xl bg-gold px-4 py-2 text-xl text-black sm:px-5 sm:text-2xl mx-auto"
+                                onClick={toggleBookingsForm}
+                            >
+                                {showBookingsForm ? "Hide Form" : "Show Form"}
+                            </button>
+                            {showBookingsForm && (
+                                <form className=" text-white">
+                                    <input type="text" name="file-name" id="name" />
+                                    <input type="file" name="file" id="files" multiple />
+                                    <button className=" mt-2 md:mt-6 md:mb-6 lg:mt-8 lg:mb-8 rounded-xl bg-gold px-4 py-2 text-xl text-black sm:px-5 sm:text-2xl mx-auto" type="submit">Submit</button>
+                                </form>
+                            )}
+                        </div>
+                    </div>
+                    <div className="bg-black overflow-hidden shadow rounded-lg border-2 border-gold">
+                        <div className="px-4 py-5 sm:p-6">
+                            <h3 className="text-lg font-medium text-white">Users</h3>
+                            <button
+                                className="mb-4 mt-2 md:mt-6 md:mb-6 lg:mt-8 lg:mb-8 rounded-xl bg-gold px-4 py-2 text-xl text-black sm:px-5 sm:text-2xl mx-auto"
+                                onClick={toggleUsersForm}
+                            >
+                                {showUsersForm ? "Hide Form" : "Show Form"}
+                            </button>
+                            {showUsersForm && (
+                                <form className=" text-white">
+                                    <input type="text" name="file-name" id="name" />
+                                    <input type="file" name="file" id="files" multiple />
+                                    <button className=" mt-2 md:mt-6 md:mb-6 lg:mt-8 lg:mb-8 rounded-xl bg-gold px-4 py-2 text-xl text-black sm:px-5 sm:text-2xl mx-auto" type="submit">Submit</button>
+                                </form>
+                            )}
+                        </div>
+                    </div>
+                    <div className="bg-black overflow-hidden shadow rounded-lg border-2 border-gold">
+                        <div className="px-4 py-5 sm:p-6">
+                            <h3 className="text-lg font-medium text-white">Screenings</h3>
+                            <button
+                                className="mb-4 mt-2 md:mt-6 md:mb-6 lg:mt-8 lg:mb-8 rounded-xl bg-gold px-4 py-2 text-xl text-black sm:px-5 sm:text-2xl mx-auto"
+                                onClick={toggleScreeningsForm}
+                            >
+                                {showScreeningsForm ? "Hide Form" : "Show Form"}
+                            </button>
+                            {showScreeningsForm && (
+                                <form className=" text-white">
+                                    <input type="text" name="file-name" id="name" />
+                                    <input type="file" name="file" id="files" multiple />
+                                    <button className=" mt-2 md:mt-6 md:mb-6 lg:mt-8 lg:mb-8 rounded-xl bg-gold px-4 py-2 text-xl text-black sm:px-5 sm:text-2xl mx-auto" type="submit">Submit</button>
+                                </form>
+                            )}
+                        </div>
+                    </div>
+                    <div className="bg-black overflow-hidden shadow rounded-lg border-2 border-gold">
+                        <div className="px-4 py-5 sm:p-6">
+                            <h3 className="text-lg font-medium text-white">Movies</h3>
+                            <button
+                                className="mb-4 mt-2 md:mt-6 md:mb-6 lg:mt-8 lg:mb-8 rounded-xl bg-gold px-4 py-2 text-xl text-black sm:px-5 sm:text-2xl mx-auto"
                                 onClick={toggleMoviesForm}
                             >
                                 {showMoviesForm ? "Hide Form" : "Show Form"}
                             </button>
                             {showMoviesForm && (
-                                <form>
+                                <form className=" text-white">
                                     <input type="text" name="file-name" id="name" />
                                     <input type="file" name="file" id="files" multiple />
-                                    <button type="submit">Submit</button>
+                                    <button className=" mt-2 md:mt-6 md:mb-6 lg:mt-8 lg:mb-8 rounded-xl bg-gold px-4 py-2 text-xl text-black sm:px-5 sm:text-2xl mx-auto" type="submit">Submit</button>
                                 </form>
                             )}
                         </div>
                     </div>
-                    <div className="bg-white overflow-hidden shadow rounded-lg">
+                    <div className="bg-black overflow-hidden shadow rounded-lg border-2 border-gold mb-8">
                         <div className="px-4 py-5 sm:p-6">
-                            <h3 className="text-lg font-medium text-gray-900">Theaters</h3>
-                            {/* Add your content for Theaters here */}
+                            <h3 className="text-lg font-medium text-white">Theaters</h3>
+                            <button
+                                className="mb-4 mt-2 md:mt-6 md:mb-6 lg:mt-8 lg:mb-8 rounded-xl bg-gold px-4 py-2 text-xl text-black sm:px-5 sm:text-2xl mx-auto"
+                                onClick={toggleTheatersForm}
+                            >
+                                {showTheatersForm ? "Hide Form" : "Show Form"}
+                            </button>
+                            {showTheatersForm && (
+                                <form className=" text-white">
+                                    <input type="text" name="file-name" id="name" />
+                                    <input type="file" name="file" id="files" multiple />
+                                    <button className=" mt-2 md:mt-6 md:mb-6 lg:mt-8 lg:mb-8 rounded-xl bg-gold px-4 py-2 text-xl text-black sm:px-5 sm:text-2xl mx-auto" type="submit">Submit</button>
+                                </form>
+                            )}
                         </div>
                     </div>
                 </div>
