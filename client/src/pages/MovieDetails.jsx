980313d6cf85e33861d83ed02a6f11b4b3176b8e
Anna Krifors <krifors@hotmail.com>
2023-10-20 16:04:16 +0200

    starting the frontend for movie details

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -1,8 +1,23 @@
-import { killersPoster, killersImage } from "../assets/index.js";
+
 import BookMovieHero from "../components/BookMovieHero.jsx";
 import MovieFilterForm from "../components/MovieFilterForm.jsx";
 import { styles } from "../styles.js";
+import { useState, useEffect } from "react";
+import { useParams } from 'react-router-dom';
+
 export default function MovieDetails() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState({})
+ 
+useEffect(() => {
+  fetch(`/api/movies/${id}`)
+  .then((res) => res.json())
+  .then((data) => {
+    setMovie(data)
+    
+  })
+},[])
+console.log(movie)
   function handleClickScroll() {
     const element = document.getElementById("scrollTo");
     console.log(element);
@@ -11,51 +26,49 @@ export default function MovieDetails() {
     }
   }
   return (
+    
     <div className=" mt-16 mb-20 h-full bg-primary">
+      
       <div className="relative h-96 md:h-[30rem] lg:h-[36rem] ">
         <img
-          src={killersImage}
+          src={""}
           alt="movie poster"
           className=" h-full min-w-full object-cover"
         />
         <img
-          src={killersPoster}
+          src={""}
           alt="movie poster"
           className="  absolute bottom-28 left-5 block h-44 sm:hidden "
         />
         <div
           className={`absolute bottom-7 left-5 p-1 lg:left-24 text-shadow`}
         >
-          <p className={`${styles.trailerTitle} `}>Lorem Ipsum</p>
-          <p className={`${styles.trailerSubTitle} `}>Romantica, Comadia</p>
+          <p className={`${styles.trailerTitle} `}>{movie.title}</p>
+          <p className={`${styles.trailerSubTitle} `}>{movie.genre}</p>
           <p className={`${styles.trailerSubTitle} bold font-inconsolata`}>
-            1 tim 33min | 11 år
+            {movie.length} | {movie.ageRestriction}
+            
           </p>
         </div>
       </div>
       <div className="relative m-auto flex flex-col p-8 sm:p-12 lg:pb-8">
         <p className={` mb-4 text-white sm:text-xl md:w-5/6 lg:w-2/3 xl:w-3/5 2xl:w-3/6 md:m-auto`}>
-          Lorem Ipsum è un testo segnaposto utilizzato nel settore della
-          tipografia e della stampa. Lorem Ipsum è considerato il testo
-          segnaposto standard sin dal sedicesimo secolo, quando un anonimo
-          tipografo prese una cassetta di caratteri e li assemblò per preparare
-          un testo campione. quando un anonimo tipografo prese una cassetta di
-          caratteri e li assemblò per preparare un.
+        {movie.description}
         </p>
         <button onClick={handleClickScroll} className="mb-4 mt-2 md:mt-6 md:mb-6 lg:mt-8 lg:mb-8 rounded-xl bg-gold px-4 py-2 text-xl text-black sm:px-5 sm:text-2xl mx-auto">
           Biljetter
         </button>
         <div className={`${styles.movieDescInfo} md:w-5/6 lg:w-2/3 xl:w-3/5 2xl:w-3/6 md:m-auto`}>
           <span>Regi:</span>
-          <span>Lorem Ipsum</span>
+          <span>{movie.director}</span>
         </div>
         <div className={`${styles.movieDescInfo} md:w-5/6 lg:w-2/3 xl:w-3/5 2xl:w-3/6 md:m-auto`}>
           <span>Skådespelare:</span>
-          <span>Testo segnaposto, Testo segnaposto</span>
+          <span>{movie.actors}</span>
         </div>
         <div className={`${styles.movieDescInfo} md:w-5/6 lg:w-2/3 xl:w-3/5 2xl:w-3/6 md:m-auto`}>
           <span>Originaltitel:</span>
-          <span>testo segnaposto</span>
+          <span>{movie.title}</span>
         </div>
         <div className={`${styles.movieDescInfo} md:w-5/6 lg:w-2/3 xl:w-3/5 2xl:w-3/6 md:m-auto`}>
           <span>Premiär:</span>
@@ -63,17 +76,19 @@ export default function MovieDetails() {
         </div>
         <div className={`${styles.movieDescInfo} md:w-5/6 lg:w-2/3 xl:w-3/5 2xl:w-3/6 md:m-auto`}>
           <span>Originalspråk</span>
-          <span>Lorem Ipsum</span>
+          <span>{movie.speech}</span>
         </div>
         <img
-          src={killersPoster}
+          src={`/img/${movie.img_poster}`}
           alt="movie poster"
           className=" absolute top-[22rem] right-[6rem] hidden h-56 sm:block lg:h-72 lg:top[22rem] lg:right-[19rem]"
         />
         <p id="scrollTo"></p>
-        <MovieFilterForm />
-        <BookMovieHero />
+
+         <MovieFilterForm />
+        <BookMovieHero /> 
       </div>
+
     </div>
   );
 }
