5c2cf9e6baefd4c0cad5e12d6fdcbe0148756dc8
Anna Krifors <krifors@hotmail.com>
2023-10-23 17:11:53 +0200

    fixed data fetch

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -2,24 +2,14 @@
 import BookMovieHero from "../components/BookMovieHero.jsx";
 import MovieFilterForm from "../components/MovieFilterForm.jsx";
 import { styles } from "../styles.js";
-import { useState, useEffect } from "react";
+
 import { useParams } from 'react-router-dom';
-import { performRequest } from "../service/fetchService.js";
+import useFetch from "../hooks/useFetch.js";
 
 export default function MovieDetails() {
   const { id } = useParams();
-  const [movie, setMovie] = useState({})
- 
-useEffect(() => {
- 
-  fetch(`/api/movies/${id}`)
-  .then((res) => res.json())
-  .then((data) => {
-    setMovie(data)
-    
-  })
-},[])
-console.log(movie)
+  const { data, isPending, error } = useFetch(`/api/movies/${id}`)
+ console.log(data)
   function handleClickScroll() {
     const element = document.getElementById("scrollTo");
     console.log(element);
@@ -28,49 +18,52 @@ console.log(movie)
     }
   }
   return (
-    
+    <>
+    {data &&(
     <div className=" mt-16 mb-20 h-full bg-primary">
-      
+  
+     
       <div className="relative h-96 md:h-[30rem] lg:h-[36rem] ">
+      
         <img
-          src={""}
+          src={`/img/${data.img_header}`}
           alt="movie poster"
           className=" h-full min-w-full object-cover"
         />
         <img
-          src={""}
+          src={`/img/${data.img_poster}`}
           alt="movie poster"
           className="  absolute bottom-28 left-5 block h-44 sm:hidden "
         />
         <div
           className={`absolute bottom-7 left-5 p-1 lg:left-24 text-shadow`}
         >
-          <p className={`${styles.trailerTitle} `}>{movie.title}</p>
-          <p className={`${styles.trailerSubTitle} `}>{movie.genre}</p>
+          <p className={`${styles.trailerTitle} `}>{data.title}</p>
+          <p className={`${styles.trailerSubTitle} `}>{data.genre}</p>
           <p className={`${styles.trailerSubTitle} bold font-inconsolata`}>
-            {movie.length} | {movie.ageRestriction}
+            {data.length} | {data.ageRestriction}
             
           </p>
         </div>
       </div>
       <div className="relative m-auto flex flex-col p-8 sm:p-12 lg:pb-8">
         <p className={` mb-4 text-white sm:text-xl md:w-5/6 lg:w-2/3 xl:w-3/5 2xl:w-3/6 md:m-auto`}>
-        {movie.description}
+        {data.description}
         </p>
         <button onClick={handleClickScroll} className="mb-4 mt-2 md:mt-6 md:mb-6 lg:mt-8 lg:mb-8 rounded-xl bg-gold px-4 py-2 text-xl text-black sm:px-5 sm:text-2xl mx-auto">
           Biljetter
         </button>
         <div className={`${styles.movieDescInfo} md:w-5/6 lg:w-2/3 xl:w-3/5 2xl:w-3/6 md:m-auto`}>
           <span>Regi:</span>
-          <span>{movie.director}</span>
+          <span>{data.director}</span>
         </div>
         <div className={`${styles.movieDescInfo} md:w-5/6 lg:w-2/3 xl:w-3/5 2xl:w-3/6 md:m-auto`}>
           <span>Skådespelare:</span>
-          <span>{movie.actors}</span>
+          <span>{data.actors}</span>
         </div>
         <div className={`${styles.movieDescInfo} md:w-5/6 lg:w-2/3 xl:w-3/5 2xl:w-3/6 md:m-auto`}>
           <span>Originaltitel:</span>
-          <span>{movie.title}</span>
+          <span>{data.title}</span>
         </div>
         <div className={`${styles.movieDescInfo} md:w-5/6 lg:w-2/3 xl:w-3/5 2xl:w-3/6 md:m-auto`}>
           <span>Premiär:</span>
@@ -78,10 +71,10 @@ console.log(movie)
         </div>
         <div className={`${styles.movieDescInfo} md:w-5/6 lg:w-2/3 xl:w-3/5 2xl:w-3/6 md:m-auto`}>
           <span>Originalspråk</span>
-          <span>{movie.speech}</span>
+          <span>{data.speech}</span>
         </div>
         <img
-          src={`/img/${movie.img_poster}`}
+          src={`/img/${data.img_poster}`}
           alt="movie poster"
           className=" absolute top-[22rem] right-[6rem] hidden h-56 sm:block lg:h-72 lg:top[22rem] lg:right-[19rem]"
         />
@@ -90,7 +83,11 @@ console.log(movie)
          <MovieFilterForm />
         <BookMovieHero /> 
       </div>
-
+   
     </div>
-  );
-}
+    )}
+    </>
+    )
+  }
+  
+ 
