e6229a3d749e10c96cb4bd99e53c7a43be2b35ea
Daniel Rotaru <113009669+Hipnosic@users.noreply.github.com>
2023-10-24 21:28:02 +0200

    Remake on the MovieCardForFilmer file, render out the movie data to movie page using props

--- client/src/components/MovieCardForFilmer.jsx
+++ client/src/components/MovieCardForFilmer.jsx
@@ -1,39 +1,42 @@
-/* DENNA TILLHÖR SIDAN FILMER*/
-import { killersImage } from "../assets";
 import { useNavigate } from "react-router-dom";
+import PropTypes from 'prop-types';
 
-export default function MovieCardForFilmer() {
+export default function MovieCardForFilmer({ movie }) {
   const navigate = useNavigate();
+
+  const handleReadMoreClick = () => {
+    navigate(`/movies/${movie.id}`);
+  };
+
 
   return (
     <div className="lg:flex lg:justify-center">
-      <div className=" lg:pr-15 flex max-w-7xl flex-col justify-between border-t-[0.5px] border-gold py-8  md:flex-row md:gap-4 md:pr-6 lg:flex-row lg:justify-start lg:gap-8">
+      <div className="lg:pr-15 flex max-w-7xl flex-col justify-between border-t-[0.5px] border-gold py-8 md:flex-row md:gap-4 md:pr-6 lg:flex-row lg:justify-start lg:gap-8">
         <img
-          src={killersImage}
-          alt="movie poster from Killers of the flower moon"
-          className="h-[250px]  w-full object-cover object-center lg:h-[450px] lg:w-1/2"
+          src={`/img/${movie.img_poster}`}
+          alt={`movie poster for ${movie.title}`}
+          className="border-[0.5px] border-gold [250px] w-full object-cover object-center lg:h-[450px] lg:w-1/2"
         />
         <div className="md: flex flex-col justify-center md:justify-around lg:justify-around">
           <div className="my-4 flex flex-col text-white-100 md:my-0 lg:my-0">
             <h2 className="font-extra-bold text-base md:text-lg lg:text-xl">
-              The Creator
+              {movie.title}
             </h2>
-            <p className="font-inconsolata text-sm lg:text-base">Sifi, drama</p>
-            <p className="flex-col pt-1 text-sm lg:text-lg">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam
-              facere totam tenetur omnis consequuntur vero ipsum, reprehenderit
-              molestiae a quae illum esse quo nulla blanditiis voluptatibus amet
-              eius? Veritatis, culpa.
-            </p>
+            <p className="font-inconsolata text-sm lg:text-base">{movie.genre}</p>
+            <p className="flex-col pt-1 text-sm lg:text-lg">{movie.description}</p>
           </div>
           <button
-            onClick={() => navigate("/movies/1")}
+            onClick={() => navigate(`/movies/${movie.id}`)}
             className="self-start rounded-md bg-gold px-4 py-2 text-black-100 lg:px-6 lg:py-3 lg:text-lg"
           >
-            Biljetter
+            Läs mer!
           </button>
         </div>
       </div>
     </div>
   );
 }
+MovieCardForFilmer.propTypes = {
+  movie: PropTypes.object.isRequired,
+};
+
--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -1,34 +1,30 @@
 import MovieCardForFilmer from "../components/MovieCardForFilmer";
-import useFetch from "../hooks/useFetch";
+import useFetch from "../hooks/useFetch"; // Import the useFetch hook
 
 export default function Movies() {
- 
   const { data, isPending, error } = useFetch("/api/movies");
   console.log(data, isPending, error);
+
   return (
     <div className="mb-20 px-8">
       <div
         className="
-      
-      mb-6 mt-20 
-      
-      text-center text-2xl
-      
-      
-       text-white 
-       
-       md:mb-12 md:mt-32 lg:mb-20 lg:mt-40"
+        mb-6 mt-20 
+        text-center text-2xl
+        text-white 
+        md-12 md:mt-32 lg-20 lg:mt-40"
       >
-        <h1 className="font-extra-bold text-xl md:text-2xl lg:text-3xl">
+        <h1 className="font-extra-bold text-gold text-xl md:text-2xl lg:text-3xl">
           Våra Filmer
         </h1>
       </div>
-      <MovieCardForFilmer />
-      <MovieCardForFilmer />
-      <MovieCardForFilmer />
-      <MovieCardForFilmer />
-      <MovieCardForFilmer />
-      <MovieCardForFilmer />
+      {data && (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {data.map((movie, index) => (
+            <MovieCardForFilmer key={index} movie={movie} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
