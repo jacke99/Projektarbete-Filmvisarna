33bf794901dc0d490dc79b2b3c4ff8d861dc2c15
jacob von scheele <jacobvonscheele@outlook.com>
2023-10-24 11:44:46 +0200

    removed console logs

--- server/src/auth/jwtFilterForm.js
+++ server/src/auth/jwtFilterForm.js
@@ -2,11 +2,9 @@ import jwtUtil from "../util/jwtUtil.js";
 
 export default function (request, response, next) {
    const authHeader = request.params.token;
-   console.log(authHeader);
    if (!authHeader || !authHeader.length > 0 || authHeader === "Bearer null") {
      response.status(400).send("Authorization header is missing");
    } else {
-    console.log("kom förbi if check");
      const authToken = authHeader.replace("Bearer ", "");
      try {
        const payload = jwtUtil.verify(authToken);
