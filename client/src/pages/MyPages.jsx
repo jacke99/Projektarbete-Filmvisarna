5bf39f04037b96731d5ef76a243db26ed8d856e1
felixlindh <113009702+felixlindh@users.noreply.github.com>
2023-10-25 11:47:35 +0200

    My pages now displays all the users bookings

--- client/src/pages/MyPages.jsx
+++ client/src/pages/MyPages.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { performRequest } from "../service/fetchService";
 import { parseJwt } from "../service/jwtService";
+import { styles } from "../styles";
 
 export default function MyPages() {
     // eslint-disable-next-line
@@ -21,26 +22,37 @@ export default function MyPages() {
     }, [])
 
     const userBookings = userData?.map((booking, index) => (
-        <div key={index} className="border-2 border-gold">
-            <ul>
-                <li>Boknings id: {booking.bookingId}</li>
-                <li>Datum: {booking.date}</li>
-                <li>Film: {booking.movie}</li>
+        <div key={index} className={`${styles.subHeaderText} text-white border-2 border-gold p-2 w-[400px]`} >
+            <ul className="flex flex-col">
+                <li className="flex justify-between"> <p className="text-gold">Boknings nr:</p> <p>{booking.bookingId}</p></li>
+                <li className="flex justify-between"> <p className="text-gold">Datum:</p> <p>{booking.screening.date}</p></li>
+                <li className="flex justify-between"> <p className="text-gold">Tid:</p> <p>{booking.screening.time}</p></li>
+                <li className="flex justify-between"> <p className="text-gold">Film:</p> <p>{booking.movie.title}</p></li>
+                <li className="flex justify-between"> <p className="text-gold">Pris:</p> <p>{booking.price}kr</p></li>
+                <li className="flex justify-between"> <p className="text-gold">Salong:</p> <p>{booking.screening.theater}</p></li>
+                <li className="flex justify-between"> <p className="text-gold">Rad:</p> <p>{booking.row}</p></li>
+                <li className="flex justify-between"> <p className="text-gold">Plats:</p> <p>{booking.seats.map((seat, i) => {
+                    if(i + 1 === booking.seats.length) {
+                        return seat.seatNumber
+                    } else {
+                        return seat.seatNumber + ","
+                    }
+                })}</p></li>
             </ul>
         </div>
     ))
     
   return (
-    <div className="mt-40 text-white">
-        <h3>Mina Sidor</h3>
-        <ul>
+    <div className="mt-20 text-white px-4">
+        <h3 className={`${styles.headerText}`} >Dina kontaktuppgifter</h3>
+        <ul className={`${styles.subHeaderText} text-white mb-10`}>
             <li>Epostadress: {currentUser.email}</li>
             <li>Namn: {currentUser.name}</li>
             <li>Efternamn: {currentUser.lastname}</li>
             <li>Telefon: {currentUser.phone}</li>
         </ul>
-        <h4>Bokningar</h4>
-        <div className="flex flex-col gap-4 items-start">
+        <h4 className={`${styles.headerText} mb-2`}>Bokningar</h4>
+        <div className="flex flex-wrap md:justify-between gap-4 items-start mb-2 justify-center">
         {userBookings}
         </div>
     </div>
