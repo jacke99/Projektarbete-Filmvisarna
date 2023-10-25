a2b3a688bdb092475f3e60673926adf9e5d74a90
Daniel Rotaru <113009669+Hipnosic@users.noreply.github.com>
2023-10-25 15:29:25 +0200

    Update on css in movie page

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -18,7 +18,7 @@ export default function Movies() {
         </h1>
       </div>
       {data && (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="">
           {data.map((movie, index) => (
             <MovieCardForFilmer key={index} movie={movie} />
           ))}
