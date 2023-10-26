b3a2bbdadb5dfb0c2b664a77b7056302ac01c464
Anna Krifors <krifors@hotmail.com>
2023-10-26 13:23:14 +0200

    fixed if no screening is avalible

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -1,7 +1,7 @@
 
 import BookMovieHero from "../components/BookMovieHero.jsx";
 import { styles } from "../styles.js";
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import useFetch from "../hooks/useFetch.js";
 import { useEffect, useState } from "react";
 import { performRequest } from "../service/fetchService.js";
@@ -96,7 +96,13 @@ export default function MovieDetails() {
       {movie.length > 0 ? (
   <BookMovieHero data={movie}/>
 ) : (
-  <p className="text-white-100">Tyvärr finns inga visningar tillgängliga just nu</p>
+  <div className="p-4 text-center">
+  <p className="text-white-100">Tyvärr finns inga visningar tillgängliga just nu
+  för {`${data.title}`}</p>
+  <Link to={"/booking"} className={`text-4-xl underline text-white-100`}>
+            Andra filmer som visas
+          </Link>
+  </div>
 )}
 
     </div>
