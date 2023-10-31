63a93f99bfebecfd67bae7a419bb82dff305ca2b
marcus nilsson <marcusnilsson2010@gmail.com>
2023-10-31 12:11:15 +0100

    working on css in movie details, would say its an good enough MVP
    co-authored-by AnnaKrifors

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -32,7 +32,7 @@ export default function MovieDetails() {
 
   // Options för att ändra storlek på videon
   const opts = {
-    height: '500',
+    height: '400',
     width: '1000',
    
   };
@@ -40,39 +40,54 @@ export default function MovieDetails() {
   return (
     <>
     {data &&(
-    <div className=" mt-10 mb-20 h-full bg-primary">
-      <div className="relative h-96 md:h-[30rem] lg:h-[36rem] ">
+     
+     
+      
+    <div className="  mb-20 h-full bg-primary  w-1/10  " >
+      <div className="md:w-5/6 lg:w-2/3 xl:w-3/5 2xl:w-3/6 md:m-auto w-1/10  ">
+          <YouTube
+        videoId={data.trailer} opts={opts} className="trailer-container  flex  md:mt-20 sm:mt-8  md:w-full md:h-[60%] lg:w-full xl:full sx:w-1/5 xl:w-[110%]"  // Use the YouTube video ID from your data
+      /></div>
+      
+      
 
         
       {/* // impelemterar en embedded youtubevideo med react-youtube */}
-      <YouTube
-        videoId={data.trailer} opts={opts} className="trailer-container flex items-center justify-center md:mt-32 sm:mt-8  md:w-full md:h-[60%] lg:w-full xl:full sx:w-1/5 xl:w-[110%]"  // Use the YouTube video ID from your data
-      />
+    
         
        {/* Alternativ 1 = kopierar hela "bädda in" från dela på youtube
       <div className="trailer-container flex items-center justify-center mt-24  md:w-full lg:w-full">
         <div dangerouslySetInnerHTML={{ __html: data.trailer }} />
       </div> */}
       
-        <img
+        {/* <img
           src={`/img/${data.img_poster}`}
           alt="movie poster"
-          className="  absolute bottom-28 left-5 block h-44 sm:hidden "
-        />
-        <div
-          className={`absolute bottom-7 left-5 p-1 lg:left-24 text-shadow`} >
+          className="  bottom-28 left-5 block h-44 sm:hidden "
+        /> */}
+      
+      
+      <div className=" m-auto flex flex-col p-8 sm:p-12 lg:pb-8">
+      <div
+          className={` text-shadow flex flex-col pb-8 sm:text-xl md:w-5/6 lg:w-2/3 xl:w-3/5 2xl:w-3/6 md:m-auto`} >
           <p className={`${styles.trailerTitle} `}>{data.title}</p>
           <p className={`${styles.trailerSubTitle} `}>{data.genre}</p>
           <p className={`${styles.trailerSubTitle} bold font-inconsolata`}>
             {data.length} | {data.ageRestriction} 
           </p>
         </div>
-      </div>
-      <div className="relative m-auto flex flex-col p-8 sm:p-12 lg:pb-8">
-        <p className={` mb-4 text-white sm:text-xl md:w-5/6 lg:w-2/3 xl:w-3/5 2xl:w-3/6 md:m-auto`}>
+        <div className="movie-poster pb-8 sm:text-xl md:w-5/6 lg:w-2/3 xl:w-3/5 2xl:w-3/6 md:m-auto" >
+        <img
+          src={`/img/${data.img_header}`}
+          alt="movie poster"
+          className="w-340 h-48 rounded-lg ]"
+        />
+        </div>
+        
+        <p className={` mb-4 text-white sm:text-xl md:w-5/6 lg:mt-14 lg:mb-10 lg:w-2/3 xl:w-3/5 2xl:w-3/6 md:m-auto`}>
         {data.description}
         </p>
-        <button onClick={handleClickScroll} className="mb-4 mt-2 md:mt-6 md:mb-6 lg:mt-8 lg:mb-8 rounded-xl bg-gold px-4 py-2 text-xl text-black sm:px-5 sm:text-2xl mx-auto">
+        <button onClick={handleClickScroll} className="mb-4 mt-10 lg:hidden  md:mt-10 md:mb-6 lg:mt-20 lg:mb-8 rounded-xl bg-gold px-4 py-2 text-xl text-black sm:px-5 sm:text-2xl mx-auto">
           Biljetter
         </button>
         <div className={`${styles.movieDescInfo} md:w-5/6 lg:w-2/3 xl:w-3/5 2xl:w-3/6 md:m-auto`}>
@@ -100,17 +115,13 @@ export default function MovieDetails() {
           <span>{data.ageRestriction}</span>
         </div>
 
-        <img
-          src={`/img/${data.img_poster}`}
-          alt="movie poster"
-          className=" absolute top-[22rem] right-[6rem] hidden h-56 sm:block lg:h-72 lg:top[22rem] lg:right-[19rem]"
-        />
-        <p id="scrollTo"></p>
-     
-            
-      </div>
-      {movie.length > 0 ? (
+        <div className="text-white text-[25px] sm:text-[25px] md:text-[30px] lg:text-[35px]  md:mt-32 lg:mt-32 mt-20 md:w-5/6 lg:w-2/3 xl:w-3/5 2xl:w-3/6 md:m-auto ">
+          <h2>Aktuell visningar </h2>
+        </div>
+        {movie.length > 0 ? (
+          <div className="md:w-5/6 lg:w-2/3 xl:w-3/5 2xl:w-3/6 md:m-auto">
   <BookMovieHero data={movie}/>
+  </div>
 ) : (
   <div className="p-4 text-center">
   <p className="text-white-100">Tyvärr finns inga visningar tillgängliga just nu
@@ -120,6 +131,12 @@ export default function MovieDetails() {
           </Link>
   </div>
 )}
+      
+        <p id="scrollTo"></p>
+     
+            
+      </div>
+ 
 
     </div>
    
