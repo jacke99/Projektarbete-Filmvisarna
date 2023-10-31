24169877023e043b6722091303cf89298b2535d1
Daniel Rotaru <113009669+Hipnosic@users.noreply.github.com>
2023-10-31 15:22:06 +0100

    cleaned up code in footer and index.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,17 +1,17 @@
-import { footerIconFB, footerIconGH, fotterIconIG, fotterIconIN } from "../assets";
+import { footerIconFB, footerIconGH, footerIconIG, footerIconIN } from "../assets";
 
 export default function Footer() {
   return (
     <footer className="px-3 bottom-0 w-full bg-[#363636] text-white">
-      <div className="flex justify-between pt-6">
+      <div className="flex justify-between pt-4">
         <div className="flex justify-start font-semibold tracking-widest">
           <h6 className="pl-1 md:text-xl lg:text-2xl">Filmvisarna</h6>
         </div>
         <div className="flex gap-4 md:gap-6 lg:gap-8 mt-1 lg:mb-4 lg:mr-2">
           <img src={footerIconFB} alt="FB_icon" className="h-3 w-3 lg:h-6 lg:w-6 md:h-4 md:w-4" />
           <img src={footerIconGH} alt="GH_icon" className="h-3 w-3 lg:h-6 lg:w-6 md:h-4 md:w-4" />
-          <img src={fotterIconIG} alt="IG_icon" className="h-3 w-3 lg:h-6 lg:w-6 md:h-4 md:w-4" />
-          <img src={fotterIconIN} alt="TW_icon" className="h-3 w-3 lg:h-6 lg:w-6 md:h-4 md:w-4" />
+          <img src={footerIconIG} alt="IG_icon" className="h-3 w-3 lg:h-6 lg:w-6 md:h-4 md:w-4" />
+          <img src={footerIconIN} alt="TW_icon" className="h-3 w-3 lg:h-6 lg:w-6 md:h-4 md:w-4" />
         </div>
       </div>
       <div className="flex w-full justify-end text-xs  mr-3.5 mb-0 mt-3 lg:absolute lg:right-1">
