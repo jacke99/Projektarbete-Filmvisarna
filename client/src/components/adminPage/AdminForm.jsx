8b88564919410ba64f62a235df31241e2d897133
marcus nilsson <marcusnilsson2010@gmail.com>
2023-10-31 15:36:41 +0100

    Working on css for pushing new movie to db
    co-authored-by AnnaKrifors

--- client/src/components/adminPage/AdminForm.jsx
+++ client/src/components/adminPage/AdminForm.jsx
@@ -1,5 +1,5 @@
 // import { useState } from "react";
-
+import { styles } from "../../styles";
 
 export default function AdminForm() {
 
@@ -8,26 +8,26 @@ export default function AdminForm() {
 
 //lägg till required på alla inputs och gör röd på alla inputs som inte är ifyllda
     return (
-        <div className="mt-20 mx-auto w-60">
+        <div className="mt-20 mx-auto w-60 " >
             <h3 className="text-lg font-medium text-white">Redigera Filmer</h3>    
             <form className=" text-black" action={`/api/movies/${authToken}`} method="post" encType="multipart/form-data">
-                <input className="border-2 border-black" type="text" name="title" placeholder="Titel..." />
-                <input className="border-2 border-black" type="text" name="desc" placeholder="Beskrivning..." />
-                <input className="border-2 border-black" type="text" name="trailer" placeholder="Trailer..." />
-                <input className="border-2 border-black" type="text" name="director" placeholder="Direktör..." />
-                <input className="border-2 border-black" type="text" name="actors" placeholder="Skådespelare..." />
-                <input className="border-2 border-black" type="text" name="length" placeholder="Längd..." />
-                <input className="border-2 border-black" type="text" name="genre" placeholder="Genre..." />
-                <input className="border-2 border-black" type="text" name="speech" placeholder="Språk..." />
-                <input className="border-2 border-black" type="text" name="subtitles" placeholder="Undertext..." />
-                <input className="border-2 border-black mb-6" type="number" name="ageRestriction" placeholder="Åldersgräns" />
+                <input className={`${styles.inputStyle}`} type="text" name="title" placeholder="Titel..." />
+                <input className={`${styles.inputStyle}`} type="text" name="desc" placeholder="Beskrivning..." />
+                <input className={`${styles.inputStyle}`} type="text" name="trailer" placeholder="Trailer..." />
+                <input className={`${styles.inputStyle}`} name="director" placeholder="Direktör..." />
+                <input className={`${styles.inputStyle}`} type="text" name="actors" placeholder="Skådespelare..." />
+                <input className={`${styles.inputStyle}`} type="text" name="length" placeholder="Längd..." />
+                <input className={`${styles.inputStyle}`} type="text" name="genre" placeholder="Genre..." />
+                <input className={`${styles.inputStyle}`} type="text" name="speech" placeholder="Språk..." />
+                <input className={`${styles.inputStyle}`} type="text" name="subtitles" placeholder="Undertext..." />
+                <input className={`${styles.inputStyle} mb-6`} type="number" name="ageRestriction" placeholder="Åldersgräns" />
         
                     
                 <label htmlFor="img_poster" className="text-white" >Huvudbild</label>
-                <input type="file" name="img_poster" id="files" multiple className="text-white" />
+                <input type="file" name="img_poster" id="files" multiple className="text-white  " />
 
                 <label htmlFor="img-header" className="text-white" >Bakgrundsbild</label>
-                <input type="file" name="img_header" id="files" multiple className="text-white" />
+                <input type="file" name="img_header" id="files" multiple className="text-white " />
                       
 
                 <button className=" mt-2 md:mt-6 md:mb-6 lg:mt-8 lg:mb-8 rounded-xl bg-gold px-4 py-2 text-xl text-black sm:px-5 sm:text-2xl mx-auto" type="submit">Skicka in</button>
