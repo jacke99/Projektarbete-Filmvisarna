2736ef3648e4caed3fd7038428b0247a2f54888d
Anna Krifors <krifors@hotmail.com>
2023-11-03 17:27:14 +0100

    "startup logic to addscreening for admin"

--- client/src/components/adminPage/AddScreeningForm.jsx
+++ client/src/components/adminPage/AddScreeningForm.jsx
@@ -1,22 +1,29 @@
 // import { useState } from "react";
+/* eslint-disable */
 import { styles } from "../../styles";
 
-export default function AddScreeningForm(){
-    const authToken = sessionStorage.getItem("AuthToken");
-    console.log(authToken)
+
+export default function AddScreeningForm( { handleSubmit, setTheater,setDate, setTime, setTitle } ){
+   
 
     return(
         <div className="mt-32 mx-auto w-60 " >
-        <h3 className="text-lg font-medium text-white">Lägg till ny filmvisning</h3>    
-        <form className=" text-black" action={`/api/screenings/${authToken}`} method="post" encType="multipart/form-data">
-            <input className={`${styles.inputStyle}`} type="date" name="date" placeholder="Datum" />
-            <input className={`${styles.inputStyle}`} type="time" name="time" placeholder="Tid" />
-            <input className={`${styles.inputStyle}`}   min="1" max="2" type="number" name="theater" placeholder="Salong" />
-            <input className={`${styles.inputStyle}`} name="" placeholder="Filmens id" />
-            <input className={`${styles.inputStyle}`} name="salongsnamn" placeholder="Salongsnamn" />
+        <h3 className="text-lg font-medium text-white mb-2">Lägg till ny filmvisning</h3>    
+        <form onSubmit= {handleSubmit} className=" text-black" method="POST">
+            <label className="text-white-100" htmlFor="date">Datum:</label>
+            <input onChange={(e) => setDate(e.target.value)} className={`${styles.inputStyle}`} type="date" name="date" placeholder="Datum" />
+            <label className="text-white-100" htmlFor="time">Tid:</label>
+            <input onChange={(e) => setTime(e.target.value)} className={`${styles.inputStyle}`} type="time" name="time" placeholder="Tid" />
+            <label className="text-white-100" htmlFor="theater">Salong:</label>
+            <input
+            className={`${styles.inputStyle}`}
+            type="number"
+            onChange={(e) => setTheater(e.target.value)}
+            min="1"
+            max="2"/>
 
-                  
-
+            <label className="text-white-100" htmlFor="title">Filmtitel:</label>
+            <input onChange={(e) => setTitle(e.target.value)} className={`${styles.inputStyle}`} name="title" placeholder="Filmens titel" />
             <button className=" mt-2 md:mt-6 md:mb-6 lg:mt-8 lg:mb-8 rounded-xl bg-gold px-4 py-2 text-xl text-black sm:px-5 sm:text-2xl mx-auto" type="submit">Skicka in</button>
         </form>
         </div>
--- client/src/pages/Admin/AdminAddScreenings.jsx
+++ client/src/pages/Admin/AdminAddScreenings.jsx
@@ -1,12 +1,40 @@
+/* eslint-disable */
 import AdminNavigation from "../../components/adminPage/AdminNavigation";
 import { Link } from "react-router-dom";
 import AddScreeningForm from "../../components/adminPage/AddScreeningForm";
+import { performRequest } from "../../service/fetchService";
+import { useState } from "react";
 
 export default function AdminAddScreenings(){
+
+    const authToken = sessionStorage.getItem("AuthToken");
+    console.log(authToken)
+    
+    const [date, setDate] = useState("");
+    const [time, setTime] = useState("")
+    const [theater, setTheater] = useState("")
+    const [title, setTitle] = useState("")
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        const formData = {date,time,theater: parseInt(theater),title}
+        console.log(formData)
+
+        try {
+            const response = await performRequest("/api/screenings", "post", formData);
+            console.log(response)
+            // Hantera responsen här
+        } catch (error) {
+            // Hantera fel här
+        }
+      }
+      
+
     return(
         <div className="mt-20">
-        <AdminNavigation/>
-            <AddScreeningForm/>
+        <AdminNavigation />
+            <AddScreeningForm handleSubmit= {handleSubmit} setTime= {setTime} setDate={setDate} setTitle = {setTitle}
+            setTheater={setTheater}/>
             <Link to="/admin/movies" className={`text-4-xl underline`}>
             ADMIN screenings
             </Link>
