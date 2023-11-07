d1eaab3165fb7e66fe9452438ce01f4482f3888a
marcus nilsson <marcusnilsson2010@gmail.com>
2023-11-07 13:08:47 +0100

    Making the input and the  icon for admin guide more dynamic. Also making different guides fo different inputs for admin. Got help with maping from jacke99
    Co-authored-by jacke99

--- client/src/components/adminPage/AddMovieForm.jsx
+++ client/src/components/adminPage/AddMovieForm.jsx
@@ -1,100 +1,66 @@
 import { useState } from "react";
 import { styles } from "../../styles";
-import { AiOutlineInfoCircle, AiOutlineCloseSquare  } from "react-icons/ai";
+import { AiOutlineInfoCircle } from "react-icons/ai";
 
 export default function AddMovieForm() {
 
-    const [showGuidetoAdminPopup, setShowGuideToAdminPopup] = useState(false)
+    const [showGuideToAdminPopup, setShowGuideToAdminPopup] = useState(false)
 
-    const trailerGuide = "Det som skrivs in på denna rad är en del av en youtublänk. Exempevis kan en kan se ut såhär  : https://www.youtube.com/watch?v=MQ9Zg24IsJY, i detta fall kopierar du allt efter '='. Vilket blir MQ9Zg24IsJY "
+    const adminGuides = {
+        Titel: "Titel = Skriv filmens titel med stor bokstav. Om svensk titel finns så ska denna skrivas.",
+        Handling: "Handling = Beskriv filmens handling.",
+        Trailer: "Trailer = Det som skrivs in på denna rad är en del av en youtublänk. Exempevis kan en se ut såhär: https://www.youtube.com/watch?v=MQ9Zg24IsJY, i detta fall kopierar du allt efter '=', vilket blir MQ9Zg24IsJY",
+        Regissör : "Regissör = Skriv namnet på regissör eller regissör. Vid flera, skriv exempelvis: Martin Scorsese, Quentin Tarantino.",
+        Skådespelare: "Skådespelare = Skriv namnet på skådespelarna enligt liknande: Robert Downey Jr, Will Smith, Michael Douglas.",
+        Längd: "Filmens längd = Skriv in filmens längd på detta vis: 2 tim 13 min.",
+        Genre: "Genre = Skriv filmens genre, exempelvis Drama. Vid flera genre, skriv Drama, Action, Thriller.",
+        Språk: "Språk = Skriv de språk de talar i filmen. Exempelvis: Svenska.",
+        Undertexter: "Undertext = Skriv de undertexter biografen erbjuder just denna film.",
+        Åldersgräns: "Åldersgräns = Vilken är filmens rekommenderade åldersgräns? Detta väljs i rullgardinsmenyn för denna rad (7, 11 eller 15 år)"
+    };
 
     const authToken = sessionStorage.getItem("AuthToken");
-    console.log(authToken)
 
-
-
-
-//lägg till required på alla inputs och gör röd på alla inputs som inte är ifyllda
     return (
         <div className="mt-32 mx-auto w-96 " >
-            <h3 className="text-lg font-medium text-white">Redigera Filmer</h3>    
+            <h3 className="text-lg font-medium text-white ml-[37%] ">Redigera Filmer</h3>    
             <form className=" relative  text-black" action={`/api/movies/${authToken}`} method="post" encType="multipart/form-data ">
-                
-                <div className="title-div flex">
-                     <AiOutlineInfoCircle className="text-white text-[25px] mt-3 mr-2" />
-                     <input className=  {`${styles.inputStyle}`} type="text" name="title" placeholder="Titel..." />
-                </div>
-                
-                <div className="description-div flex">
-                    <AiOutlineInfoCircle className="text-white text-[25px] mt-3 mr-2" />
-                    <input className={`${styles.inputStyle}`} type="text" name="desc" placeholder="Beskrivning..."  />
-                </div>
-                
-                <div className="trailer-div flex">
-                <AiOutlineInfoCircle
-                    onMouseEnter={() => setShowGuideToAdminPopup(true)}
-                    onMouseLeave={() => setShowGuideToAdminPopup(false)}
-                    className="text-white text-[25px] mt-3 mr-2 cursor-pointer"
-                />
-                    <input className={`${styles.inputStyle}`} type="text" name="trailer" placeholder="Trailer..."  />
 
-                    {/* Show trailer guide to ADMIN */}
-                {showGuidetoAdminPopup && (
-                    <div className="absolute right-[400px] top-8">
-                        <div className=" bg-white text-black p-5 text-center rounded-lg flex flex-col items-end">
-                            <div className="close_icon-div mb-4 text-[20px] ">
-                            </div>
-                            <p className="">{trailerGuide}</p>
-                            
-                        </div>
+                {Object.keys(adminGuides).map((inputName, index)=>(
+                    <div className={`${inputName}-div flex`} key={index}>
+                        <AiOutlineInfoCircle
+                        onMouseEnter={()=> setShowGuideToAdminPopup(inputName)}
+                        onMouseLeave={()=> setShowGuideToAdminPopup(null)}
+                        className="text-white text-[25px] mt-3 mr-2 cursor-pointer transition-transform transform hover:scale-[1.3]"
+                        />
+                        
+                        { inputName !== "Åldersgräns"?
+                         <input className={`${styles.inputStyle}`} type="text" name={inputName} placeholder={inputName}/>:
+                         <input className={`${styles.inputStyle} mb-6`} type="number" name={inputName} placeholder={inputName} />
+                         }
+
+                        {showGuideToAdminPopup === inputName && (
+                               <div className="absolute right-[400px] top- w-80">
+                               <div className="bg-white text-black p-5 text-center rounded-lg flex flex-col items-end border-[5px] border-gold">
+                                   <p>{adminGuides[inputName]}</p>
+                               </div>
+                           </div>
+                        )}
                     </div>
-                    )}
-                </div>
-
-                <div className="director-div flex">
-                    <AiOutlineInfoCircle className="text-white text-[25px] mt-3 mr-2" />
-                     <input className={`${styles.inputStyle}`} name="director" placeholder="Direktör..." />
-                </div>
-               
-                <div className="actors-div flex">
-                    <AiOutlineInfoCircle className="text-white text-[25px] mt-3 mr-2" />
-                    <input className={`${styles.inputStyle}`} type="text" name="actors" placeholder="Skådespelare..." />
-                </div>
-
-                <div className="lenght-div flex">
-                     <AiOutlineInfoCircle className="text-white text-[25px] mt-3 mr-2" />
-                     <input className={`${styles.inputStyle}`} type="text" name="length" placeholder="Längd..." />
+                )
+            )}
+                <div className="mt-5 ml-8 " >
+                     <label htmlFor="img_poster" className="text-white " >Huvudbild</label>
+                     <input type="file" name="img_poster" id="files" multiple className="text-white mb-5 " />
                 </div>
                 
-                <div className="genre-div flex">
-                    <AiOutlineInfoCircle className="text-white text-[25px] mt-3 mr-2" />
-                    <input className={`${styles.inputStyle}`} type="text" name="genre" placeholder="Genre..." />
+                <div className="ml-8">
+                    <label htmlFor="img-header" className="text-white" >Bakgrundsbild</label>
+                    <input type="file" name="img_header" id="files" multiple className="text-white " />
                 </div>
+         
 
-                <div className="sppech-div flex">
-                    <AiOutlineInfoCircle className="text-white text-[25px] mt-3 mr-2" />
-                    <input className={`${styles.inputStyle}`} type="text" name="speech" placeholder="Språk..." />
-                </div>
-
-                <div className="subtitles-div flex">
-                    <AiOutlineInfoCircle className="text-white text-[25px] mt-3 mr-2" />
-                    <input className={`${styles.inputStyle}`} type="text" name="subtitles" placeholder="Undertext..." />
-                </div>
-                
-                <div className="ageRestriction-div flex">
-                    <AiOutlineInfoCircle className="text-white text-[25px] mt-3 mr-2" />
-                    <input className={`${styles.inputStyle} mb-6`} type="number" name="ageRestriction" placeholder="Åldersgräns" />
-                </div>
-        
-                    
-                <label htmlFor="img_poster" className="text-white" >Huvudbild</label>
-                <input type="file" name="img_poster" id="files" multiple className="text-white mb-10" />
-
-                <label htmlFor="img-header" className="text-white" >Bakgrundsbild</label>
-                <input type="file" name="img_header" id="files" multiple className="text-white " />
-                      
-
-                <button className=" mt-2 md:mt-6 md:mb-6 lg:mt-8 lg:mb-8 rounded-xl bg-gold px-4 py-2 text-xl text-black sm:px-5 sm:text-2xl mx-auto" type="submit">Skicka in</button>
+                <button className=" ml-[30%] mt-2 md:mt-6 md:mb-6 lg:mt-8 lg:mb-8 rounded-xl bg-gold px-4 py-2 text-xl text-black sm:px-5 sm:text-2xl mx-auto" type="submit">Skicka in</button>
             </form>
 
         </div>
