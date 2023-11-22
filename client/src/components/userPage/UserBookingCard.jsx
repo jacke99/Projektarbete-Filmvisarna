8af40ba5fa8fa0bc1d88aa6bc6fbc1850d9dd559
jacob von scheele <jacobvonscheele@outlook.com>
2023-11-22 14:02:40 +0100

    fixed missing key issue in my pages and formatted code

--- client/src/components/userPage/UserBookingCard.jsx
+++ client/src/components/userPage/UserBookingCard.jsx
@@ -1,81 +1,134 @@
 import { expandMore, expandLess } from "../../assets";
 import { AnimatePresence, motion } from "framer-motion";
 import newDateFormat from "../../service/newDateFormat";
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
 import { useState } from "react";
 
-export default function UserBookingCard({ booking, setCancelBooking, setToggle }) {
+export default function UserBookingCard({
+  booking,
+  setCancelBooking,
+  setToggle,
+  index,
+}) {
   // const [toggleExpand, setToggleExpand] = useState(false)
   const [isExpanded, setExpanded] = useState(false);
 
   function cancel(booking) {
-    setCancelBooking(booking)
-    setToggle(true)
+    setCancelBooking(booking);
+    setToggle(true);
   }
 
- 
-return(
-  <motion.div 
-  className={` flex flex-col mx-auto mdd:ml-8 text-white rounded-lg  relative`}>
-  <div className="w-full max-w-[17rem] xs:max-w-[24rem] xs:mx-0">
-
- 
-  <motion.div className="flex flex-col xs:flex-row xs:items-end">
-    <img 
-      src={`/img/${booking.movie.img_poster}`} 
-      alt={`poster of the movie: ${booking.movie.title}`} 
-      className={`w-[10rem] rounded-lg mx-auto xs:mx-0`}
-    />
-    <div className="flex flex-col gap-1 mt-4 xs:mb-0">
-      <p className="text-md md:text-xl ml-4">{booking.movie?.title}</p>
-      <p className="text-base md:text-lg ml-4">{booking.status ? newDateFormat(booking.screening?.date): booking.screening?.date}</p> 
-      <p className="text-base md:text-lg ml-4">Klockan {booking.screening?.time}</p> 
-      <p className="text-base md:text-lg ml-4">BokningsNr: {booking.bookingId}</p>
-      <div className="flex whitespace-nowrap w-20 mb-2 mx-auto cursor-pointer underline xs:mb-0" onClick={() => setExpanded(!isExpanded)}>
-        {isExpanded ? "Mindre" : "Mer info"}
-        {isExpanded && <img className="w-8" src={expandLess} alt="expand less icon" />}
-        {!isExpanded && <img className="w-8" src={expandMore} alt="expand more icon" />}
-      </div>
-    </div>
-  </motion.div>
-  <AnimatePresence>
-  {isExpanded && (
-    <motion.div 
-      className={`text-base mt-2 w-full mx-auto`}
-      initial={{opacity: 0, y: -30}}
-      animate={{opacity: 1, y: 0}}
-      exit={{opacity: 0, y: -30}}
-      transition={{
-        duration: .5,
-      }}>
-        <p className="flex justify-between"> <p className="text-gold md:text-lg">Salong:</p> <p className="md:text-lg">{booking.screening?.theaterName}</p></p>
-        <p className="flex justify-between"> <p className="text-gold md:text-lg">Rad:</p> <p className="md:text-lg">{`${booking.rows ? booking.rows?.map((row) => row.row).join(", "): booking.row}`}</p></p>
-        <p className="flex justify-between"> <p className="text-gold md:text-lg">Plats:</p> <p className="md:text-lg">{`${booking.seats?.map((seat) => seat.seatNumber).join(", ")}`}</p></p>
-        <p className="flex justify-between"> <p className="text-gold md:text-lg">Pris:</p> <p className="md:text-lg">{booking.price} kr</p></p>
-        <p className="flex justify-between"> <p className="text-gold md:text-lg">Status:</p> <p className="md:text-lg">{booking.status ? "Bokad" : "Inaktiv"}</p></p>
-        <div className="flex justify-center">
-        {booking.status ? <button disabled={!booking.status} className="bg-red-600 px-3 py-1 rounded-lg mt-4" onClick={() => cancel(booking)}>Avboka</button> : "" }
+  return (
+    <motion.div
+      key={index}
+      className={`relative mx-auto flex flex-col rounded-lg text-white  mdd:ml-8`}
+    >
+      <div className="w-full max-w-[17rem] xs:mx-0 xs:max-w-[24rem]">
+        <motion.div className="flex flex-col xs:flex-row xs:items-end">
+          <img
+            src={`/img/${booking.movie.img_poster}`}
+            alt={`poster of the movie: ${booking.movie.title}`}
+            className={`mx-auto w-[10rem] rounded-lg xs:mx-0`}
+          />
+          <div className="mt-4 flex flex-col gap-1 xs:mb-0">
+            <p className="text-md ml-4 md:text-xl">{booking.movie?.title}</p>
+            <p className="ml-4 text-base md:text-lg">
+              {booking.status
+                ? newDateFormat(booking.screening?.date)
+                : booking.screening?.date}
+            </p>
+            <p className="ml-4 text-base md:text-lg">
+              Klockan {booking.screening?.time}
+            </p>
+            <p className="ml-4 text-base md:text-lg">
+              BokningsNr: {booking.bookingId}
+            </p>
+            <div
+              className="mx-auto mb-2 flex w-20 cursor-pointer whitespace-nowrap underline xs:mb-0"
+              onClick={() => setExpanded(!isExpanded)}
+            >
+              {isExpanded ? "Mindre" : "Mer info"}
+              {isExpanded && (
+                <img className="w-8" src={expandLess} alt="expand less icon" />
+              )}
+              {!isExpanded && (
+                <img className="w-8" src={expandMore} alt="expand more icon" />
+              )}
+            </div>
+          </div>
+        </motion.div>
+        <AnimatePresence>
+          {isExpanded && (
+            <motion.div
+              className={`mx-auto mt-2 w-full text-base`}
+              initial={{ opacity: 0, y: -30 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -30 }}
+              transition={{
+                duration: 0.5,
+              }}
+            >
+              <div className="flex justify-between">
+                {" "}
+                <p className="text-gold md:text-lg">Salong:</p>{" "}
+                <p className="md:text-lg">{booking.screening?.theaterName}</p>
+              </div>
+              <div className="flex justify-between">
+                {" "}
+                <p className="text-gold md:text-lg">Rad:</p>{" "}
+                <p className="md:text-lg">{`${
+                  booking.rows
+                    ? booking.rows?.map((row) => row.row).join(", ")
+                    : booking.row
+                }`}</p>
+              </div>
+              <div className="flex justify-between">
+                {" "}
+                <p className="text-gold md:text-lg">Plats:</p>{" "}
+                <p className="md:text-lg">{`${booking.seats
+                  ?.map((seat) => seat.seatNumber)
+                  .join(", ")}`}</p>
+              </div>
+              <div className="flex justify-between">
+                {" "}
+                <p className="text-gold md:text-lg">Pris:</p>{" "}
+                <p className="md:text-lg">{booking.price} kr</p>
+              </div>
+              <div className="flex justify-between">
+                {" "}
+                <p className="text-gold md:text-lg">Status:</p>{" "}
+                <p className="md:text-lg">
+                  {booking.status ? "Bokad" : "Inaktiv"}
+                </p>
+              </div>
+              <div className="flex justify-center">
+                {booking.status ? (
+                  <button
+                    disabled={!booking.status}
+                    className="mt-4 rounded-lg bg-red-600 px-3 py-1"
+                    onClick={() => cancel(booking)}
+                  >
+                    Avboka
+                  </button>
+                ) : (
+                  ""
+                )}
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </motion.div>
-    
-  )}
-    </AnimatePresence>
-    </div>
-</motion.div>
-)
+  );
 }
 
-  
-    
-
-
 UserBookingCard.propTypes = {
-    booking: PropTypes.object,
-    setCancelBooking: PropTypes.func,
-    setToggle: PropTypes.func,
-    index: PropTypes.number,
-    columnIndex: PropTypes.number,
-    expandedStates: PropTypes.array,
-    setExpandedStates: PropTypes.func
-
-  }
+  booking: PropTypes.object,
+  setCancelBooking: PropTypes.func,
+  setToggle: PropTypes.func,
+  index: PropTypes.number,
+  columnIndex: PropTypes.number,
+  expandedStates: PropTypes.array,
+  setExpandedStates: PropTypes.func,
+  key: PropTypes.number,
+};
