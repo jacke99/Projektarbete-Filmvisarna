a06a46c01dcb2c913340f7d0500126395cc3adf3
felixlindh <113009702+felixlindh@users.noreply.github.com>
2023-11-22 10:59:48 +0100

    you can now see the prices of the differnt tickettypes in the first part of the booking flow. Also when booking seperate seats you can now book more than 6 tickets

--- client/src/components/bookMovie/TicketCounter.jsx
+++ client/src/components/bookMovie/TicketCounter.jsx
@@ -1,109 +1,176 @@
 /* eslint-disable */
-import {useStates} from "react-easier"
-import PropTypes from "prop-types"
+import { useStates } from "react-easier";
+import PropTypes from "prop-types";
 import newDateFormat from "../../service/newDateFormat";
 
 export default function TicketCounter({ screening, movie, seats, setSeats }) {
   const counters = useStates("ticketCounter");
-  const s = useStates("toggleSeparateSeats")
+  const s = useStates("toggleSeparateSeats");
   function increaseCounters(e) {
-    const {name} = e.target;
-    if(eval(counters.adult + counters.child + counters.senior) < 6) {
-      counters[name]++
-      counters.total++
-      const array = [...seats]
-      
-      if(array.length > 0 && !s.toggle) {
-        
-        if(array.length % 2 == 0 && seats[seats.length - 1].seat + 1 <= screening.seats[seats[0].row - 1].length  && !screening.seats[seats[0].row - 1][seats[seats.length - 1].seat].seat) {
-          array.push({row: seats[seats.length - 1].row, seat: seats[seats.length - 1].seat + 1, seatNumber: screening.seats[seats[0].row - 1][seats[seats.length - 1].seat].seatNumber, booked: screening.seats[seats[0].row - 1][seats[seats.length - 1].seat].seat})
-         return setSeats(array)
-          
-        } else if(seats[0].seat - 1 !== 0 && !screening.seats[seats[0].row - 1][seats[0].seat - 2].seat) {
-          array.unshift({row: seats[seats.length - 1].row, seat: seats[0].seat - 1, seatNumber: screening.seats[seats[0].row - 1][seats[0].seat - 2].seatNumber, booked: screening.seats[seats[0].row - 1][seats[0].seat - 1].seat})
-         return setSeats(array)
-        } else if (!screening.seats[seats[0].row - 1][seats[seats.length - 1].seat].seat) {
-          array.push({row: seats[seats.length - 1].row, seat: seats[seats.length - 1].seat + 1, seatNumber: screening.seats[seats[0].row - 1][seats[seats.length - 1].seat].seatNumber, booked: screening.seats[seats[0].row - 1][seats[seats.length - 1].seat].seat})
-         return setSeats(array)
-          
+    const { name } = e.target;
+    if (counters.adult + counters.child + counters.senior < 6 || s.toggle) {
+      counters[name]++;
+      counters.total++;
+      const array = [...seats];
+
+      if (array.length > 0 && !s.toggle) {
+        if (
+          array.length % 2 == 0 &&
+          seats[seats.length - 1].seat + 1 <=
+            screening.seats[seats[0].row - 1].length &&
+          !screening.seats[seats[0].row - 1][seats[seats.length - 1].seat].seat
+        ) {
+          array.push({
+            row: seats[seats.length - 1].row,
+            seat: seats[seats.length - 1].seat + 1,
+            seatNumber:
+              screening.seats[seats[0].row - 1][seats[seats.length - 1].seat]
+                .seatNumber,
+            booked:
+              screening.seats[seats[0].row - 1][seats[seats.length - 1].seat]
+                .seat,
+          });
+          return setSeats(array);
+        } else if (
+          seats[0].seat - 1 !== 0 &&
+          !screening.seats[seats[0].row - 1][seats[0].seat - 2].seat
+        ) {
+          array.unshift({
+            row: seats[seats.length - 1].row,
+            seat: seats[0].seat - 1,
+            seatNumber:
+              screening.seats[seats[0].row - 1][seats[0].seat - 2].seatNumber,
+            booked: screening.seats[seats[0].row - 1][seats[0].seat - 1].seat,
+          });
+          return setSeats(array);
+        } else if (
+          !screening.seats[seats[0].row - 1][seats[seats.length - 1].seat].seat
+        ) {
+          array.push({
+            row: seats[seats.length - 1].row,
+            seat: seats[seats.length - 1].seat + 1,
+            seatNumber:
+              screening.seats[seats[0].row - 1][seats[seats.length - 1].seat]
+                .seatNumber,
+            booked:
+              screening.seats[seats[0].row - 1][seats[seats.length - 1].seat]
+                .seat,
+          });
+          return setSeats(array);
         }
       }
-      setSeats(array)
+      setSeats(array);
     }
   }
   function decreaseCounters(e) {
-    const {name} = e.target;
+    const { name } = e.target;
     if (counters[name] > 0) {
-      counters[name]--
-      counters.total--
-      const array = [...seats]
-      if(array.length % 2 == 0 && !s.toggle) {
-        array.pop()
+      counters[name]--;
+      counters.total--;
+      const array = [...seats];
+      if (array.length % 2 == 0 && !s.toggle) {
+        array.pop();
       } else {
-        array.shift()
+        array.shift();
       }
-      setSeats(array)
+      setSeats(array);
     }
   }
   return (
-    <div className="flex flex-col text-center text-xl text-white sm:flex-row justify-evenly sm:mt-40 sm:text-2xl p-8 lg:w-[80%]">
+    <div className="flex flex-col justify-evenly p-8 text-center text-xl text-white sm:mt-40 sm:flex-row sm:text-2xl lg:w-[80%]">
       <div className="sm:text-3xl lg:w-2/5">
-        <h2 className="mb-6 text-3xl sm:text-3xl lg:text-left lg:text-[2.5rem]">Välj antal biljetter</h2>
+        <h2 className="mb-6 text-3xl sm:text-3xl lg:text-left lg:text-[2.5rem]">
+          Välj antal biljetter
+        </h2>
         <div className="lg:flex lg:items-center lg:justify-between">
-          <p className="sm:text-2xl">Vuxen</p>
+          <p className="sm:text-2xl">Vuxen (140 kr)</p>
           <div className=" flex items-center justify-center">
-            <button className="text-4xl sm:text-5xl" name="adult" onClick={(e) => decreaseCounters(e)}>
+            <button
+              className="text-4xl sm:text-5xl"
+              name="adult"
+              onClick={(e) => decreaseCounters(e)}
+            >
               -
             </button>
             <p className="mx-3 text-2xl sm:text-3xl">{counters.adult}</p>
-            <button className="text-3xl sm:text-4xl" name="adult" onClick={(e) => increaseCounters(e)}>
+            <button
+              className="text-3xl sm:text-4xl"
+              name="adult"
+              onClick={(e) => increaseCounters(e)}
+            >
               +
             </button>
           </div>
         </div>
         <div className="lg:flex lg:items-center lg:justify-between">
-          <p className="sm:text-2xl">Barn</p>
+          <p className="sm:text-2xl">Barn (120 kr)</p>
           <div className="flex items-center justify-center">
-            <button className="text-4xl sm:text-5xl" name="child" onClick={(e) => decreaseCounters(e)}>
+            <button
+              className="text-4xl sm:text-5xl"
+              name="child"
+              onClick={(e) => decreaseCounters(e)}
+            >
               -
             </button>
             <p className="mx-3 text-2xl sm:text-3xl">{counters.child}</p>
-            <button className="text-3xl sm:text-4xl" name="child" onClick={(e) => increaseCounters(e)}>
+            <button
+              className="text-3xl sm:text-4xl"
+              name="child"
+              onClick={(e) => increaseCounters(e)}
+            >
               +
             </button>
           </div>
         </div>
         <div className="lg:flex lg:items-center lg:justify-between">
-          <p className="sm:text-2xl">Pensionär</p>
+          <p className="sm:text-2xl">Pensionär (80 kr)</p>
           <div className="flex items-center justify-center">
-            <button className="text-4xl sm:text-5xl" name="senior" onClick={(e) => decreaseCounters(e)}>
+            <button
+              className="text-4xl sm:text-5xl"
+              name="senior"
+              onClick={(e) => decreaseCounters(e)}
+            >
               -
             </button>
             <p className="mx-3 text-2xl sm:text-3xl">{counters.senior}</p>
-            <button className="text-3xl sm:text-4xl" name="senior" onClick={(e) => increaseCounters(e)}>
+            <button
+              className="text-3xl sm:text-4xl"
+              name="senior"
+              onClick={(e) => increaseCounters(e)}
+            >
               +
             </button>
           </div>
         </div>
       </div>
-      {screening && movie && <div className="flex gap-6 mt-6">
-        <img className="h-44 sm:h-60 sm:mt-auto" src={`/img/${movie.img_poster}`} alt="movie poster" />
-        <div className="flex flex-col justify-end text-start text-base">
-          <p className="text-xl">{movie.title}</p>
-          <p>{`${newDateFormat(screening.date).replaceAll("/", "-")} | ${screening.time}`}</p>
-          <p>{screening.theaterName}</p>
-          <p>{movie.genre}</p>
-          <p>{movie.length} </p>
-          <p>{movie.ageRestriction === 0 ?  " Ingen åldersgräns" :  ` ${movie.ageRestriction} år`}</p>
-          
+      {screening && movie && (
+        <div className="mt-6 flex gap-6">
+          <img
+            className="h-44 sm:mt-auto sm:h-60"
+            src={`/img/${movie.img_poster}`}
+            alt="movie poster"
+          />
+          <div className="flex flex-col justify-end text-start text-base">
+            <p className="text-xl">{movie.title}</p>
+            <p>{`${newDateFormat(screening.date).replaceAll("/", "-")} | ${
+              screening.time
+            }`}</p>
+            <p>{screening.theaterName}</p>
+            <p>{movie.genre}</p>
+            <p>{movie.length} </p>
+            <p>
+              {movie.ageRestriction === 0
+                ? " Ingen åldersgräns"
+                : ` ${movie.ageRestriction} år`}
+            </p>
+          </div>
         </div>
-      </div>}
+      )}
     </div>
   );
 }
 
-
 TicketCounter.propTypes = {
   screening: PropTypes.object,
   movie: PropTypes.object,
-}
+};
--- client/src/components/moviesPage/MovieCard.jsx
+++ client/src/components/moviesPage/MovieCard.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { styles } from "../../styles";
-import PropTypes from 'prop-types';
-import { useState } from 'react';
+import PropTypes from "prop-types";
+import { useState } from "react";
 export default function MovieCard({ movie }) {
   const navigate = useNavigate();
   const [showFullDescription, setShowFullDescription] = useState(false);
@@ -16,32 +16,41 @@ export default function MovieCard({ movie }) {
   const renderDescription = () => {
     const maxLength = 80;
     const shortDescription = movie.description.slice(0, maxLength);
-    const readMore = <p className="underline inline-block"> Läs mer</p>
-    return showFullDescription ? (movie.description) : (<>{shortDescription}... {readMore}</>);
+    const readMore = <p className="inline-block underline"> Läs mer</p>;
+    return showFullDescription ? (
+      movie.description
+    ) : (
+      <>
+        {shortDescription}... {readMore}
+      </>
+    );
   };
 
   return (
     <div className="lg:flex lg:justify-start">
-      <div className="flex max-w-7xl flex-col justify-between border-t-[0.5px] border-gold py-4 md:py-8 md:flex-row md:gap-4  lg:flex-row lg:justify-start lg:gap-8">
+      <div className="flex max-w-7xl flex-col justify-between border-t-[0.5px] border-gold py-4 md:flex-row md:gap-4 md:py-8  lg:flex-row lg:justify-start lg:gap-8">
         <img
           src={`/img/${movie.img_poster}`}
           alt={`movie poster for ${movie.title}`}
-          className="object-cover sm:h-56 lg:h-72 rounded-xl hidden md:block w-[150px]"
+          className="hidden w-[200px] rounded-xl object-cover sm:h-56 md:block md:w-[200px] lg:h-72"
         />
         <img
           src={`/img/${movie.img_header}`}
           alt={`movie poster for ${movie.title}`}
-          className="object-cover h-56 md:hidden  w-[calc(100%_+_24px)]"
+          className="h-56 w-[calc(100%_+_24px)] object-cover  md:hidden"
         />
         <div className="md: flex flex-col justify-center md:justify-around lg:justify-around">
           <div className="my-4 flex flex-col text-white-100">
-            <h2 className={`${styles.subHeaderText}`}>
-              {movie.title}
-            </h2>
+            <h2 className={`${styles.subHeaderText}`}>{movie.title}</h2>
             <p className={`${styles.subText}`}>{movie.genre}</p>
-            <p className={`${styles.bodyText} hidden md:block`}>{movie.description}</p>
-            <p className="flex-col pt-1 text-sm lg:text-lg md:hidden" onClick={toggleDescription}>
-            {renderDescription()}
+            <p className={`${styles.bodyText} hidden md:block`}>
+              {movie.description}
+            </p>
+            <p
+              className="flex-col pt-1 text-sm md:hidden lg:text-lg"
+              onClick={toggleDescription}
+            >
+              {renderDescription()}
             </p>
           </div>
           <button
